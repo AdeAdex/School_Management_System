f3f8f5ac1ab3833634493270ae54fe7cf1b5f0dd
Adeolu Amole <adeoluamole@gmail.com>
2023-04-27 17:15:49 +0100

    Adding other components to the landing / home page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import Carousel from "./components/Carousel";
 import NavigateTo from "./components/NavigateTo";
 import OurActivities from "./components/OurActivities";
-import './Homepage.scss'
-import './App.css'
+import "./Homepage.scss";
+import "./App.css";
+import Countdown from "./components/Countdown";
 
 const HomePage = () => {
   const [count, setCount] = useState(0);
@@ -13,69 +14,188 @@ const HomePage = () => {
     setCalculation(() => count * 2);
   }, [count]);
 
+  var countDownDate = new Date("May 5, 2023 15:37:25").getTime();
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    tm1.innerHTML = days + "d ";
+    tm2.innerHTML = hours + "h ";
+    tm3.innerHTML = minutes + "m ";
+    tm4.innerHTML = seconds + "s ";
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+
   return (
     <>
       <Carousel />
-      <div className="mx-auto" style={{ width: "75%" }}>
-        <section className="d-flex flex-column flex-lg-row justify-content-center">
-          <NavigateTo
-            txt="courses"
-            style={{
-              backgroundColor: "#74CEE4",
-              borderTopLeftRadius: "5px",
-              borderBottomLeftRadius: "5px",
-            }}
-          />
-          <NavigateTo txt="prices" style={{ backgroundColor: "#edbf47" }} />
-          <NavigateTo txt="events" style={{ backgroundColor: "#ec774b" }} />
-          <NavigateTo
-            txt="teachers"
-            style={{
-              backgroundColor: "#6fc191",
-              borderTopRightRadius: "5px",
-              borderBottomRightRadius: "5px",
-            }}
-          />
-        </section>
-        <div className="activities "><h2 className="">our activities</h2> <p>OUR BEST SERVICES FOR YOUR KIDS</p></div>
-        <h1 className="text-4xl font-bold underline text-red-500 text-center">Hello world!</h1>
-        <section className="activities-container mb-5 pb-5 d-flex flex-wrap w-100">        
-          <OurActivities
-            title="creative"
-            icon="fas fa-lightbulb text-primary fs-1"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
-          />
-          <OurActivities
-            title="many sport"
-            icon="fas fa-person-skiing fs-1"
-            style={{ color: "#EC774B" }}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
-          />
-          <OurActivities
-            title="bus free"
-            icon="fas fa-bus fs-1"
-            style={{ color: "#CDDC39" }}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
-          />
-          <OurActivities
-            title="modern canteen"
-            icon="fas fa-utensils fs-1"
-            style={{ color: "#FFC107" }}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
-          />
-          <OurActivities
-            title="friendly support"
-            icon="fas fa-hand-holding-heart fs-1"
-            style={{ color: "#3F51B5" }}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
-          />
-          <OurActivities
-            title="excursion"
-            icon="fas fa-earth-europe fs-1"
-            style={{ color: "#FF5722" }}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
-          />
-        </section>
+      <div className="mx-auto" style={{ width: "100%", height: "100vh" }}>
+        <div className="center-div">
+          <section className="d-flex flex-column flex-lg-row justify-content-center w-100">
+            <NavigateTo
+              txt="courses"
+              style={{
+                backgroundColor: '#74CEE4',
+                borderTopLeftRadius: "5px",
+                borderBottomLeftRadius: "5px",
+              }}
+            />
+            <NavigateTo txt="prices" style={{ backgroundColor: "#edbf47" }} />
+            <NavigateTo txt="events" style={{ backgroundColor: "#ec774b" }} />
+            <NavigateTo
+              txt="teachers"
+              style={{
+                backgroundColor: "#6fc191",
+                borderTopRightRadius: "5px",
+                borderBottomRightRadius: "5px",
+              }}
+            />
+          </section>
+        </div>
+        <div class="bg image1"></div>
+        <div className="center-div">
+          <div>
+            <div className="activities ">
+              <h2 className="">our activities</h2>{" "}
+              <p>OUR BEST SERVICES FOR YOUR KIDS</p>
+            </div>
+            <section className="activities-container mb-5 pb-5 d-flex flex-wrap w-100">
+              <OurActivities
+                title="creative"
+                icon="fas fa-lightbulb text-primary fs-1"
+                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+              />
+              <OurActivities
+                title="many sport"
+                icon="fas fa-person-skiing fs-1"
+                style={{ color: "#EC774B" }}
+                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+              />
+              <OurActivities
+                title="bus free"
+                icon="fas fa-bus fs-1"
+                style={{ color: "#CDDC39" }}
+                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+              />
+              <OurActivities
+                title="modern canteen"
+                icon="fas fa-utensils fs-1"
+                style={{ color: "#FFC107" }}
+                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+              />
+              <OurActivities
+                title="friendly support"
+                icon="fas fa-hand-holding-heart fs-1"
+                style={{ color: "#3F51B5" }}
+                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+              />
+              <OurActivities
+                title="excursion"
+                icon="fas fa-earth-europe fs-1"
+                style={{ color: "#FF5722" }}
+                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+              />
+            </section>
+          </div>
+        </div>
+        <div class="bg image2 d-flex">
+          <p className="center-div text-white my-auto  h-50 d-flex justify-content-between">
+            <Countdown id="tm1" styles={{backgroundColor: '#74CEE4'}}></Countdown>
+            <Countdown id="tm2" styles={{backgroundColor: '#edbf47'}}></Countdown>
+            <Countdown id="tm3" styles={{backgroundColor: '#ec774b'}}></Countdown>
+            <Countdown id="tm4" styles={{backgroundColor: '#6fc191'}}></Countdown>
+          </p>
+        </div>
+        <div className="center-div">
+          <section className="activities-container mb-5 pb-5 d-flex flex-wrap w-100">
+            <OurActivities
+              title="creative"
+              icon="fas fa-lightbulb text-primary fs-1"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="many sport"
+              icon="fas fa-person-skiing fs-1"
+              style={{ color: "#EC774B" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="bus free"
+              icon="fas fa-bus fs-1"
+              style={{ color: "#CDDC39" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="modern canteen"
+              icon="fas fa-utensils fs-1"
+              style={{ color: "#FFC107" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="friendly support"
+              icon="fas fa-hand-holding-heart fs-1"
+              style={{ color: "#3F51B5" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="excursion"
+              icon="fas fa-earth-europe fs-1"
+              style={{ color: "#FF5722" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+          </section>
+        </div>
+        <div class="bg image3"></div>
+        <div className="center-div">
+          <section className="activities-container mb-5 pb-5 d-flex flex-wrap w-100">
+            <OurActivities
+              title="creative"
+              icon="fas fa-lightbulb text-primary fs-1"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="many sport"
+              icon="fas fa-person-skiing fs-1"
+              style={{ color: "#EC774B" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="bus free"
+              icon="fas fa-bus fs-1"
+              style={{ color: "#CDDC39" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="modern canteen"
+              icon="fas fa-utensils fs-1"
+              style={{ color: "#FFC107" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="friendly support"
+              icon="fas fa-hand-holding-heart fs-1"
+              style={{ color: "#3F51B5" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+            <OurActivities
+              title="excursion"
+              icon="fas fa-earth-europe fs-1"
+              style={{ color: "#FF5722" }}
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            />
+          </section>
+        </div>
+        <div class="bg image4"></div>
       </div>
       {/* <div
         className="position-fixed top-0 d-flex flex-column gap-2"
---
+++ src/components/Countdown.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Countdown = ({id, styles}) => {
+  return (
+        <div className="margin-y-auto" id={id} style={styles}></div>
+  )
+}
+
+export default Countdown
