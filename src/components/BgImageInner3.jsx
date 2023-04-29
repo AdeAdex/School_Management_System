64195b74a5272cf3504de83c5bc830537fb56ad8
Adeolu Amole <adeoluamole@gmail.com>
2023-04-29 22:07:46 +0100

    Working on the landing / home page

--- src/components/BgImageInner3.jsx
+++ src/components/BgImageInner3.jsx
@@ -4,8 +4,8 @@ import CalculateTotalNumber from './CalculateTotalNumber'
 const BgImageInner3 = () => {
   return (
     <>
-        <p className="center-div" style={{ height: "60%" }}>
-            <div className="center-div2 d-flex col-lg-10 justify-content-between">
+        <p className="bg-image3-main center-div" style={{ height: "60%" }}>
+            <div className="bg-image3-container center-div2 d-flex col-lg-10 justify-content-between">
             <CalculateTotalNumber innerText='teachers' styles={{backgroundColor: '#74CEE4'}} text='54'></CalculateTotalNumber>
             <CalculateTotalNumber innerText='courses' styles={{backgroundColor: '#6fc191'}} text='32'></CalculateTotalNumber>
             <CalculateTotalNumber innerText='students' styles={{backgroundColor: '#edbf47'}} text='78'></CalculateTotalNumber>
