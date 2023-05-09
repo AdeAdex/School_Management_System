ad27877c273029b2a82490f69ae31ab4a63d24bc
Adeolu Amole <adeoluamole@gmail.com>
2023-05-09 17:27:04 +0100

    Working on the about teachers page

--- src/components/Contact_Teacher_Form.jsx
+++ src/components/Contact_Teacher_Form.jsx
@@ -8,7 +8,9 @@ const Contact_Teacher_Form = () => {
           {/* <input type="text" required="" placeholder="NAME" className="text-capitalize" /> */}
           <input type="email" className='w-100 border-0 py-2 px-3' id="email" name="email" required="" placeholder="EMAIL" autoComplete="off"style={{backgroundColor: '#F9F9F9'}} />
           <textarea id="message" name="message" required="" placeholder="MESSAGE" className="text-capitalize border-0  py-2 px-3" style={{backgroundColor: '#F9F9F9', height: '8em', resize: 'none'}}></textarea>
+          <div className='w-100 justify-content-end d-flex'>
           <button type="submit" className='submit-btn btn btn-sm py-2 skyblue col-3 text-white text-uppercase'>send</button>
+          </div>
         </form>
       </div>
     </>
