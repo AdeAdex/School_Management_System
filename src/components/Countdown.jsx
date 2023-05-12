ab2ec3a7326f761b63836e5b6d7e533d7c850342
Adeolu Amole <adeoluamole@gmail.com>
2023-05-12 12:16:53 +0100

    Working on the medium screen responsiveness

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Countdown = ({id, styles, innerText}) => {
+const Countdown = ({ id, styles, innerText }) => {
   return (
     <>
-    <div>
-    <div className="margin-y-auto" id={id}></div>
-        <div className="text-center text-uppercase rounded py-1" style={styles}>{innerText}</div>
-    </div>
-    
+      <div className="">
+        <div className="margin-y-auto" id={id}></div>
+        <div className="text-center text-uppercase rounded py-1" style={styles}>
+          {innerText}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Countdown
+export default Countdown;
