bd2e13b13853c0f7e63926a392b7a181d7471e61
Adeolu Amole <adeoluamole@gmail.com>
2023-05-21 22:52:30 +0100

    Working on the sign in page

--- src/components/signinComponents/SigninHero.jsx
+++ src/components/signinComponents/SigninHero.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import { useMediaQuery } from 'react-responsive'
-
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import { Link } from "react-router-dom";
 
 const SigninHero = () => {
   const isLargeScreen = useMediaQuery({ query: "(min-width: 768px)" });
 
   return (
     <>
-    <div
-        className="h-100 py-4 px-5 d-flex flex-column position-relative text-white signin-header"
-      >
-        <h2 className="mt-lg-5 header-text" style={{ textTransform: "capitalize" }}>
-        hi dear
+      <div className="h-100 py-4 px-5 d-flex flex-column position-relative text-white signin-header">
+        <Link to="/student_signup" style={{color: 'white'}}><i className="fas fa-circle-chevron-left" style={{ fontSize: "30px" }} ></i></Link> 
+        <h2
+          className="mt-lg-5 header-text"
+          style={{ textTransform: "capitalize" }}
+        >
+          hi dear
         </h2>
-        <h5>
-          To keep connecting with us,
-        </h5>
-        <div>
-        Please login with your personal info
-        </div>
-          {/* {isLargeScreen ? <SignupCarousel /> : null} */}
-        
+        <h5>To keep connecting with us,</h5>
+        <div>Please login with your personal info</div>
+        {/* {isLargeScreen ? <SignupCarousel /> : null} */}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SigninHero
+export default SigninHero;
