754a5594c38e9fed5c6a4b813ecb062089b1b499
Adeolu Amole <adeoluamole@gmail.com>
2023-05-21 02:10:52 +0100

    Working on the sign in page

--- src/components/signinComponents/SigninForm.jsx
+++ src/components/signinComponents/SigninForm.jsx
@@ -47,14 +47,29 @@ const SigninForm = () => {
               onChange={formik.handleChange}
             />
             <label>
-              <span style={{ transitionDelay: "0ms" }}>U</span>
-              <span style={{ transitionDelay: "50ms" }}>s</span>
-              <span style={{ transitionDelay: "100ms" }}>e</span>
-              <span style={{ transitionDelay: "150ms" }}>r</span>
-              <span style={{ transitionDelay: "200ms" }}>n</span>
-              <span style={{ transitionDelay: "250ms" }}>a</span>
-              <span style={{ transitionDelay: "300ms" }}>m</span>
-              <span style={{ transitionDelay: "350ms" }}>e</span>
+              <span style={{ transitionDelay: "0ms" }}>E</span>
+              <span style={{ transitionDelay: "50ms" }}>m</span>
+              <span style={{ transitionDelay: "100ms" }}>a</span>
+              <span style={{ transitionDelay: "150ms" }}>i</span>
+              <span style={{ transitionDelay: "200ms" }}>l</span>
+            </label>
+          </div>
+          <div className="form-control">
+            <input
+              type="password"
+              required="required"
+              name="password"
+              onChange={formik.handleChange}
+            />
+            <label>
+              <span style={{ transitionDelay: "0ms" }}>P</span>
+              <span style={{ transitionDelay: "50ms" }}>a</span>
+              <span style={{ transitionDelay: "100ms" }}>s</span>
+              <span style={{ transitionDelay: "150ms" }}>s</span>
+              <span style={{ transitionDelay: "200ms" }}>w</span>
+              <span style={{ transitionDelay: "250ms" }}>o</span>
+              <span style={{ transitionDelay: "300ms" }}>r</span>
+              <span style={{ transitionDelay: "350ms" }}>d</span>
             </label>
           </div>
 
