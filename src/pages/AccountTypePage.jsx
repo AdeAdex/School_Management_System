07d0d9b699eba95ce88639aaea90e78309fdfc9d
Adeolu Amole <adeoluamole@gmail.com>
2023-05-23 17:51:23 +0100

    Working on the account type page

--- src/pages/AccountTypePage.jsx
+++ src/pages/AccountTypePage.jsx
@@ -1,27 +1,75 @@
 import React, { useState } from "react";
 import PagesNavbar from "../components/navbarComponents/PagesNavbar";
+import { useNavigate } from "react-router-dom";
 
 const AccountTypePage = (props) => {
-  const [first, setfirst] = useState("")
-       const setImage = (num) => {
-        if (num === 1) {
-          setfirst("Hello Staff you are welcome, kindly click the button below to creat an account")
-        } else {
-          setfirst("Hi Student you are welcome, kindly click the button below to creat an account")
-        }
-       } 
+  const [first, setfirst] = useState("");
+  const setImage = (num) => {
+    if (num === 1) {
+      setfirst(
+        "Hello Staff you are welcome, kindly click the button below to create an account"
+      );
+    } else {
+      setfirst(
+        "Hi Student you are welcome, kindly click the button below to create an account"
+      );
+    }
+  };
+
+  const navigate  = useNavigate()
+  const createAccount = () => {
+    if (first === "Hello Staff you are welcome, kindly click the button below to create an account") {
+      navigate("/staff_signup")
+    } else {
+      navigate("/student_signup")
+    }
+  };
   return (
     <>
-    <PagesNavbar/>
-      <div className="d-flex flex-column justify-content-center w-100" style={{height: '100vh'}}>
+      <PagesNavbar />
+      <div
+        className="d-flex flex-column justify-content-center w-100"
+        style={{ height: "100vh" }}
+      >
         <div className="m-auto w-50 shadow h-50 px-5 py-3 d-flex flex-column justify-content-center">
-          <h3 className="text-capitalize text-center" style={{fontFamily:" Georgia, 'Times New Roman', Times, serif", color: '#01F'}}>choose account type</h3>
+          <h3
+            className="text-capitalize text-center"
+            style={{
+              fontFamily: " Georgia, 'Times New Roman', Times, serif",
+              color: "#01F",
+            }}
+          >
+            choose account type
+          </h3>
           <div className="d-flex gap-4 mx-auto mt-5">
-            <img src="pic/avatar.png" alt=""  style={{ width: "100px", outline: 'outset gray', cursor: 'pointer' }} onClick={() => setImage(1)} />
-            <img src="pic/ade.png" alt="" style={{ width: "100px", outline: 'outset gray', cursor: 'pointer' }} onClick={() => setImage(2)}/>
+            <img
+              src="pic/avatar.png"
+              alt=""
+              style={{
+                width: "100px",
+                outline: "outset gray",
+                cursor: "pointer",
+              }}
+              onClick={() => setImage(1)}
+            />
+            <img
+              src="pic/ade.png"
+              alt=""
+              style={{
+                width: "100px",
+                outline: "outset gray",
+                cursor: "pointer",
+              }}
+              onClick={() => setImage(2)}
+            />
           </div>
           <div className="text-center mt-5">{first}</div>
-          <button className="btn btn-sm px-5 mt-4 bg-primary text-white mx-auto">Continue</button>
+          <button
+            className="btn btn-sm px-5 mt-4 bg-primary text-white mx-auto"
+            onClick={createAccount}
+          >
+            Continue
+          </button>
         </div>
       </div>
     </>
