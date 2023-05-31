ccf0790bc4086a7552e030cd562199e623119e46
Adeolu Amole <adeoluamole@gmail.com>
2023-05-31 17:26:53 +0100

    Working on the staff dashboard page

--- src/components/staffSigninComponents/StaffSigninForm.jsx
+++ src/components/staffSigninComponents/StaffSigninForm.jsx
@@ -14,7 +14,7 @@ const StaffSigninForm = () => {
     },
 
     onSubmit: (values) => {
-      const endpoint = "https://school-potal-back-end-adex2210.vercel.app/staff_account/staff_signin";
+      const endpoint = "https://school-potal-back-end-adex2210.vercel.app";
       axios.post(endpoint, values)
       .then((res) => {
         console.log(res.data);
