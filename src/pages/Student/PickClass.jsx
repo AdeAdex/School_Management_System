d4440a0f461d25ab16787ef087f4d81c8f64ac1f
Adeolu Amole <adeoluamole@gmail.com>
2023-06-03 14:58:21 +0100

    Working on the student sign up page

--- src/pages/Student/PickClass.jsx
+++ src/pages/Student/PickClass.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const PickClass = () => {
   return (
-    <div>PickClass</div>
+    <>
+      <div>
+        <input type="text" name="" id="" />
+        <div className="col-md-12 mb-3">
+            <label htmlFor="validationServer04" className="form-label">
+              State
+            </label>
+            <select
+              className="form-select "
+              id="validationServer04"
+              name="state"
+              aria-describedby="validationServer04Feedback"
+              required
+              onChange={(e) => setState(e.target.value)}
+            >
+              <option disabled>
+                Choose...
+              </option>
+              <option value="JSS 1">JSS 1</option>
+              <option value="JSS 2">JSS 2</option>
+              <option value="JSS 3">JSS 3</option>
+              <option value="SSS 1">SSS 1</option>
+              <option value="SSS 2">SSS 2</option>
+              <option value="SSS 3">SSS 3</option>
+            </select>
+            <div id="validationServer04Feedback" className="invalid-feedback">
+              Please select a valid state.
+            </div>
+          </div>
+      </div>
+    </>
   )
 }
 
