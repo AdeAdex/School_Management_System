9eac8940fab135f99e04d5d8c502659e97ef9bfe
Adeolu Amole <adeoluamole@gmail.com>
2023-06-03 14:29:52 +0100

    Working on the student sign up page

--- src/pages/Student/CreateAccount.jsx
+++ src/pages/Student/CreateAccount.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
-import { Route, Router, Routes, redirect, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Route, Router, Routes, redirect, useNavigate } from "react-router-dom";
 
 const CreateAccount = () => {
-
-        const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -17,86 +16,75 @@ const CreateAccount = () => {
   // const [state, setState] = useState("");
 
   const createAccount = (e) => {
-//     let myDetails = {
-//       firstName: firstName,
-//       lastName: lastName,
-//       email: email,
-//       phoneNumber: phoneNumber,
-//       password: password,
-//       check: false,
-//       // city: city,
-//       // age: age,
-//       // gender: gender,
-//       // state: state,
-//       // matricNo: "",
-//     };
-    
-navigate('/student_signup/admission')
+        let myDetails = {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phoneNumber: phoneNumber,
+          password: password,
+          check: false,
+          // city: city,
+          // age: age,
+          // gender: gender,
+          // state: state,
+          // matricNo: "",
+        };
 
-    // e.preventDefault();
-    // const endpoint = "http://localhost:2000/student_account/student_signup";
-    // axios.post(endpoint, myDetails)
-    // .then((response) => {
-    //   if (response.data == "Information saved to the student database") {
-    //     navigate("/student_signin");
-    //   }
-    // });
+        
+        e.preventDefault();
+        const endpoint = "http://localhost:2000/student_account/student_signup";
+        axios.post(endpoint, myDetails)
+        .then((response) => {
+          if (response.data == "Information saved to the student database") {
+            navigate("/student_signup/admission");
+      }
+    });
   };
 
   return (
     <>
-    
-      {/* <Router>
-          <Routes>
-            <Route path="pick_class" element={<></>}/>
-          </Routes>
-        </Router> */}
-        <h2 className="fw-bold fs-2" style={{ textTransform: "capitalize" }}>
-          student sign up
-        </h2>
-        <h6 className="d-flex gap-5 mt-3">
-          <span className="mt-auto"> Have an account already </span>
-          <a href="/student_signin" className="fs-4 my-auto">
-            Login
-          </a>
-        </h6>
-       
-          <div className="col-md-6 position-relative  flex-column mb-3">
-            <input
-              type="text"
-              autoComplete="on"
-              className="input form-control "
-              id=""
-              name="firstName"
-              required
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <label htmlFor="validationServer01" className="user-label">
-              First Name
-            </label>
-            <div className="invalid-feedback">
-              Please provide your firstName!
-            </div>
-          </div>
-          <div className="col-md-6 position-relative  flex-column mb-3">
-            <input
-              type="text"
-              autoComplete="on"
-              className="input form-control "
-              id=""
-              name="lastName"
-              required
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <label htmlFor="validationServer01" className="user-label">
-              Last Name
-            </label>
-            <div className="invalid-feedback">
-              Please provide your lastName!
-            </div>
-          </div>
-          
-          {/* <div className="col-md-6 position-relative d-flex flex-column mb-3">
+      <h2 className="fw-bold fs-2" style={{ textTransform: "capitalize" }}>
+        student sign up
+      </h2>
+      <h6 className="d-flex gap-5 mt-3">
+        <span className="mt-auto"> Have an account already </span>
+        <a href="/student_signin" className="fs-4 my-auto">
+          Login
+        </a>
+      </h6>
+      <form action="" method="post">
+        <div className="col-md-6 position-relative  flex-column mb-3">
+          <input
+            type="text"
+            autoComplete="on"
+            className="input form-control "
+            id=""
+            name="firstName"
+            required
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <label htmlFor="validationServer01" className="user-label">
+            First Name
+          </label>
+          <div className="invalid-feedback">Please provide your firstName!</div>
+        </div>
+        <div className="col-md-6 position-relative  flex-column mb-3">
+          <input
+            type="text"
+            autoComplete="on"
+            className="input form-control "
+            id=""
+            name="lastName"
+            required
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <label htmlFor="validationServer01" className="user-label">
+            Last Name
+          </label>
+          <div className="invalid-feedback">Please provide your lastName!</div>
+        </div>
+
+        {/* <div className="col-md-6 position-relative d-flex flex-column mb-3">
             <input
               type="text"
               autoComplete="on"
@@ -113,7 +101,7 @@ navigate('/student_signup/admission')
               Please provide a valid city.
             </div>
           </div> */}
-          {/* <div className="col-md-6 position-relative d-flex flex-column mb-3">
+        {/* <div className="col-md-6 position-relative d-flex flex-column mb-3">
             <input
               type="number"
               autoComplete="on"
@@ -130,7 +118,7 @@ navigate('/student_signup/admission')
               Please provide a your age.
             </div>
           </div> */}
-          {/* <div className="col-md-6 position-relative d-flex flex-column mb-3">
+        {/* <div className="col-md-6 position-relative d-flex flex-column mb-3">
             <input
               type="text"
               autoComplete="on"
@@ -147,24 +135,24 @@ navigate('/student_signup/admission')
               Please provide a your gender.
             </div>
           </div> */}
-          <div className="col-lg-12 position-relative d-flex flex-column mb-3">
-            <input
-              type="email"
-              autoComplete="on"
-              className="input form-control "
-              id=""
-              name="email"
-              required
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="validationServer01" className="user-label">
-              Email Address
-            </label>
-            <div id="validationServer04Feedback" className="invalid-feedback">
-              Please provide a valid email address.
-            </div>
+        <div className="col-lg-12 position-relative d-flex flex-column mb-3">
+          <input
+            type="email"
+            autoComplete="on"
+            className="input form-control "
+            id=""
+            name="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="validationServer01" className="user-label">
+            Email Address
+          </label>
+          <div id="validationServer04Feedback" className="invalid-feedback">
+            Please provide a valid email address.
           </div>
-          {/* <div className="col-md-12 mb-3">
+        </div>
+        {/* <div className="col-md-12 mb-3">
             <label htmlFor="validationServer04" className="form-label">
               State
             </label>
@@ -186,70 +174,71 @@ navigate('/student_signup/admission')
               Please select a valid state.
             </div>
           </div> */}
-          <div className="col-md-6 position-relative d-flex flex-column mb-3">
+        <div className="col-md-6 position-relative d-flex flex-column mb-3">
+          <input
+            type="tel"
+            autoComplete="on"
+            className="input form-control "
+            id=""
+            name="phoneNumber"
+            required
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <label htmlFor="validationServer01" className="user-label">
+            Phone Number
+          </label>
+          <div id="validationServer04Feedback" className="invalid-feedback">
+            Please provide a valid phone number.
+          </div>
+        </div>
+        <div className="col-lg-6 position-relative d-flex flex-column mb-3">
+          <input
+            type="text"
+            autoomplete="on"
+            className="input form-control "
+            id=""
+            name="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="validationServer01" className="user-label">
+            Password
+          </label>
+          <div id="validationServer04Feedback" className="invalid-feedback">
+            Please provide a valid password.
+          </div>
+        </div>
+        <div className="col-12">
+          <div className="form-check">
             <input
-              type="tel"
-              autoComplete="on"
-              className="input form-control "
-              id=""
-              name="phoneNumber"
+              className="form-check-input is-invalid"
+              type="checkbox"
+              id="invalidCheck3"
+              aria-describedby="invalidCheck3Feedback"
               required
-              onChange={(e) => setPhoneNumber(e.target.value)}
+              name="check"
+              onChange={(e) => setCheck(e.target.checked)}
             />
-            <label htmlFor="validationServer01" className="user-label">
-              Phone Number
+            <label className="form-check-label" htmlFor="invalidCheck3">
+              Agree to terms and conditions
             </label>
-            <div id="validationServer04Feedback" className="invalid-feedback">
-              Please provide a valid phone number.
+            <div id="invalidCheck3Feedback" className="invalid-feedback">
+              You must agree before submitting.
             </div>
           </div>
-          <div className="col-lg-6 position-relative d-flex flex-column mb-3">
-            <input
-              type="text"
-              autoomplete="on"
-              className="input form-control "
-              id=""
-              name="password"
-              required
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label htmlFor="validationServer01" className="user-label">
-              Password
-            </label>
-            <div id="validationServer04Feedback" className="invalid-feedback">
-              Please provide a valid password.
-            </div>
-          </div>
-          <div className="col-12">
-            <div className="form-check">
-              <input
-                className="form-check-input is-invalid"
-                type="checkbox"
-                id="invalidCheck3"
-                aria-describedby="invalidCheck3Feedback"
-                required
-                name="check"
-                onChange={(e) => setCheck(e.target.checked)}
-              />
-              <label className="form-check-label" htmlFor="invalidCheck3">
-                Agree to terms and conditions
-              </label>
-              <div id="invalidCheck3Feedback" className="invalid-feedback">
-                You must agree before submitting.
-              </div>
-            </div>
-          </div>
-          <div className="col-12">
-            <button
-              className="btn btn-primary signup-btn"
-              type="submit"
-              onClick={createAccount}
-            >
-              Create Account
-            </button>
-          </div>
+        </div>
+        <div className="col-12">
+          <button
+            className="btn btn-primary signup-btn"
+            type="submit"
+            onClick={createAccount}
+          >
+            Create Account
+          </button>
+        </div>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default CreateAccount
+export default CreateAccount;
