cf2c753ee2f6421e1d36b5c3b987c64f42d8e019
Adeolu Amole <adeoluamole@gmail.com>
2023-06-05 01:37:07 +0100

    Working on the student sign up page

--- src/pages/Student/CreateAccount.jsx
+++ src/pages/Student/CreateAccount.jsx
@@ -37,15 +37,30 @@ const CreateAccount = () => {
         navigate("/student/admission");
       }
       else {
-      console.log(eresponse.data.message);
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'error',
+          title: response.data.message
+        })
       }
     })
-    .catch((err) => {
-      // console.log(response.data.message);
-      if (err.code === 11000) {
-        // alert(err.message);r
-      }
-    })
+    // .catch((err) => {
+    //   console.log(response.data.message);
+    //   if (err.code === 11000) {
+    //     // alert(err.message);r
+    //   }
+    // })
     },
 
     validationSchema: yup.object({
