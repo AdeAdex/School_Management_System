897c9a8dabd4458e1a8ad44642a6a84be6a03893
Adeolu Amole <adeoluamole@gmail.com>
2023-06-05 02:14:04 +0100

    Working on the student sign up page

--- src/pages/Student/Admission.jsx
+++ src/pages/Student/Admission.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, NavLink, Route, Router, Routes } from "react-router-dom";
 import PickClass from "./PickClass";
 import Payment from "./Payment";
@@ -13,6 +13,11 @@ const Admission = () => {
       textDecoration: isActive ? "none" : "underline",
     };
   };
+  // useEffect(() => {
+  //   dispatch(newName(response.data.response));
+   
+  // }, [])
+  
   return (
     <>
       <div className="font-bold ml-4 my-auto text-lg text-danger">{globalState.firstName} {globalState.lastName}</div>
