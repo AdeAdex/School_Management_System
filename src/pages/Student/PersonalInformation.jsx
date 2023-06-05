fcd4c1d87f93fb752bb70a6a06699a386c70ebbd
Adeolu Amole <adeoluamole@gmail.com>
2023-06-05 16:52:20 +0100

    Working on the student sign up page

--- src/pages/Student/PersonalInformation.jsx
+++ src/pages/Student/PersonalInformation.jsx
@@ -9,10 +9,13 @@ const PersonalInformation = () => {
     initialValues: {
       firstName: globalState.firstName,
       lastName: globalState.lastName,
-      city: "",
-      age: "",
-      gender: "",
-      email: globalState.email
+      city: globalState.city,
+      age: globalState.age,
+      gender: globalState.gender,
+      email: globalState.email,
+      state: globalState.state,
+      country: globalState.country,
+      phoneNumber: globalState.phoneNumber,
     }
   })
   return (
@@ -51,7 +54,7 @@ const PersonalInformation = () => {
             required
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={formik}
+            value={formik.values.lastName}
           />
           <label htmlFor="validationServer01" className="user-label">
             Last Name
@@ -67,7 +70,8 @@ const PersonalInformation = () => {
               id=""
               name="city"
               required
-              onChange={(e) => setCity(e.target.value)}
+              onChange={formik.handleChange}
+              value={formik.values.city}
             />
             <label htmlFor="validationServer01" className="user-label">
               City
@@ -84,7 +88,8 @@ const PersonalInformation = () => {
               id=""
               name="age"
               required
-              onChange={(e) => setAge(e.target.value)}
+              onChange={formik.handleChange}
+              value={formik.values.age}
             />
             <label htmlFor="validationServer01" className="user-label">
               Age
@@ -101,7 +106,8 @@ const PersonalInformation = () => {
               id=""
               name="gender"
               required
-              onChange={(e) => setGender(e.target.value)}
+              onChange={formik.handleChange}
+              value={formik.values.gender}
             />
             <label htmlFor="validationServer01" className="user-label">
               Gender
@@ -110,7 +116,7 @@ const PersonalInformation = () => {
               Please provide a your gender.
             </div>
           </div>
-        <div className="col-lg-12 position-relative  flex-column mb-3">
+        <div className="col-lg-6 position-relative  flex-column mb-3">
           <input
             type="email"
             autoComplete="on"
@@ -120,6 +126,7 @@ const PersonalInformation = () => {
             required
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            value={formik.values.email}
           />
           <label htmlFor="validationServer01" className="user-label">
             Email Address
@@ -128,7 +135,7 @@ const PersonalInformation = () => {
             Please provide a valid Email address.
           </div>
         </div>
-        <div className="col-md-12 mb-3">
+        <div className="col-md-6 mb-3">
             <label htmlFor="validationServer04" className="form-label">
               State
             </label>
@@ -138,13 +145,39 @@ const PersonalInformation = () => {
               name="state"
               aria-describedby="validationServer04Feedback"
               required
-              onChange={(e) => setState(e.target.value)}
+              onChange={formik.handleChange}
+              value={formik.values.state}
             >
               <option disabled>
                 Choose...
               </option>
               <option value="Oyo State">Oyo State</option>
               <option value="Lagos State">Lagos State</option>
+            </select>
+            <div id="validationServer04Feedback" className="invalid-feedback">
+              Please select a valid state.
+            </div>
+          </div>
+          <div className="col-md-6 mb-3">
+            <label htmlFor="validationServer04" className="form-label">
+              Country
+            </label>
+            <select
+              className="form-select "
+              id="validationServer04"
+              name="country"
+              aria-describedby="validationServer04Feedback"
+              required
+              onChange={formik.handleChange}
+              value={formik.values.country}
+            >
+              <option disabled>
+                Choose...
+              </option>
+              <option value="Nigeria">Nigeria</option>
+              <option value="Uk">United Kingdom</option>
+              <option value="USA">United State of America</option>
+              <option value="Canada">Canada</option>
             </select>
             <div id="validationServer04Feedback" className="invalid-feedback">
               Please select a valid state.
@@ -160,6 +193,7 @@ const PersonalInformation = () => {
             required
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
+            value={formik.values.phoneNumber}
           />
           <label htmlFor="validationServer01" className="user-label">
             Phone Number
