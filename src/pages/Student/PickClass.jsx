b396a85b6733c5c49b889397c71962d7e15ad6c2
Adeolu Amole <adeoluamole@gmail.com>
2023-06-05 09:56:06 +0100

    Working on the student sign up page

--- src/pages/Student/PickClass.jsx
+++ src/pages/Student/PickClass.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react'
-
+import axios from "axios";
+import { useFormik } from "formik";
+import React, { useState } from "react";
 
 const PickClass = () => {
-  const [state, setState] = useState("")
+  // const [state, setState] = useState("")
+  let formik = useFormik({
+    initialValues: {
+      state: "",
+    },
+
+    onSubmit: (values) => {
+      console.log(values);
+      // const endpoint = "http://localhost:2000/student_account/student"
+      // axios.post(endpoint, values)
+      // .then((response) => {
+      //   console.log(response)
+      // })
+    },
+  });
 
   return (
     <>
-      <div>
-        <input type="text" name="" id="" />
-        <div className="col-md-12 mb-3">
+      <div className="shadow p-4 mt-4">
+        <form action="" method="post" onSubmit={formik.handleSubmit}>
+          <div className="col-md-12 mb-3">
             <label htmlFor="validationServer04" className="form-label">
-              State
+              Course
             </label>
             <select
               className="form-select "
@@ -18,11 +33,11 @@ const PickClass = () => {
               name="state"
               aria-describedby="validationServer04Feedback"
               required
-              onChange={(e) => setState(e.target.value)}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              // onChange={(e) => setState(e.target.value)}
             >
-              <option disabled>
-                Choose...
-              </option>
+              <option disabled>Choose...</option>
               <option value="JSS 1">JSS 1</option>
               <option value="JSS 2">JSS 2</option>
               <option value="JSS 3">JSS 3</option>
@@ -34,9 +49,60 @@ const PickClass = () => {
               Please select a valid state.
             </div>
           </div>
+          <div className="col-md-12 mb-3">
+            <label htmlFor="validationServer04" className="form-label">
+              Course
+            </label>
+            <select
+              className="form-select "
+              id="validationServer04"
+              name="state"
+              aria-describedby="validationServer04Feedback"
+              required
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              // onChange={(e) => setState(e.target.value)}
+            >
+              <option disabled>Choose...</option>
+              <option value="JSS 1">Science</option>
+              <option value="JSS 2">Commercial</option>
+              <option value="JSS 3">Art</option>
+            </select>
+            <div id="validationServer04Feedback" className="invalid-feedback">
+              Please select a valid state.
+            </div>
+          </div>
+          <div className="col-md-12 mb-3">
+            <label htmlFor="validationServer04" className="form-label">
+              Level
+            </label>
+            <select
+              className="form-select "
+              id="validationServer04"
+              name="state"
+              aria-describedby="validationServer04Feedback"
+              required
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              // onChange={(e) => setState(e.target.value)}
+            >
+              <option disabled>Choose...</option>
+              <option value="JSS 1">JSS 1</option>
+              <option value="JSS 2">JSS 2</option>
+              <option value="JSS 3">JSS 3</option>
+              <option value="SSS 1">SSS 1</option>
+              <option value="SSS 2">SSS 2</option>
+              <option value="SSS 3">SSS 3</option>
+            </select>
+            <div id="validationServer04Feedback" className="invalid-feedback">
+              Please select a valid state.
+            </div>
+          </div>
+          <button type="submit">Continue</button>
+        </form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default PickClass
+export default PickClass;
