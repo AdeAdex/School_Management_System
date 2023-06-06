5bbd12ff20c8f10c66d4eccdd7714c5edee1ace4
Adeolu Amole <adeoluamole@gmail.com>
2023-06-06 14:12:16 +0100

    Working on the student sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ function App() {
         <Route path='/' element={<HomePage/>}/>
         <Route path='/:username' element={username? <UserPage/> : <Navigate to="/*"/>}/>
 
-        <Route path='/student_signin' element={<StudentSignIn/>}/>
+        <Route path='/student_signin' element={studentToken ? <Navigate to="/student_dashboard"/> : <StudentSignIn/>}/>
         <Route path='/student/*' element={ <StudentSignUp/>}>
           <Route path='create_account' element={<CreateAccount/>}/>
           <Route path='admission/*' element={<Admission/>}>
