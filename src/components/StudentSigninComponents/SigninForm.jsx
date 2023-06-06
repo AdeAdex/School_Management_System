c1ce43e9f4e52249ab971ef0fc9adbb8ce86d81e
Adeolu Amole <adeoluamole@gmail.com>
2023-06-06 16:23:44 +0100

    Working on the student sign up page

--- src/components/StudentSigninComponents/SigninForm.jsx
+++ src/components/StudentSigninComponents/SigninForm.jsx
@@ -23,6 +23,22 @@ const SigninForm = () => {
           navigate("/student_dashboard");
         } else {
           navigate("/student_signin");
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top",
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
+
+          Toast.fire({
+            icon: "error",
+            title: res.data.message,
+          });
         }
       });
     },
