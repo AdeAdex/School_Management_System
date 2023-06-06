8629b4807c5ab83107bfc6382db92c2fc2cec3c5
Adeolu Amole <adeoluamole@gmail.com>
2023-06-06 12:11:10 +0100

    Working on the student sign up page

--- src/components/studentDashboardComponents/StudentDashboardOffcanvas.jsx
+++ src/components/studentDashboardComponents/StudentDashboardOffcanvas.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import StudentDashboardOffcanvasTitle from './StudentDashboardOffcanvasTitle'
+import StudentDashboardOffcanvasList from './StudentDashboardOffcanvasList'
 
 const StudentDashboardOffcanvas = () => {
   return (
-    <div>StudentDashboardOffcanvas</div>
+    <>
+      <div
+        className="position-relative"
+        id=""
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div className="dashboard-offcanvas w-100 position-relative text-white">
+          <div
+            className="w-full flex justify-center shadow border-b-2"
+            style={{ height: "80px", width: "100%" }}
+          >
+            <img
+              src="pic/ade.png"
+              alt=""
+              className="my-auto"
+              style={{ width: "60px", height: "60px" }}
+            />
+          </div>
+          <div
+            className="capitalize text-center py-6 text-lg font-bold"
+            id="menu"
+          >
+            personal menu
+          </div>
+          <div className="px-4 grid gap-y-12">
+            <div className="grid gap-y-5">
+              <StudentDashboardOffcanvasTitle title="main menu" />
+              <StudentDashboardOffcanvasList item="dashboard" params="/staff_dashboard/home" icons="fas fa-border-all" />
+            </div>
+            <div className="grid gap-y-5">
+              <StudentDashboardOffcanvasTitle title="profile" />
+              <StudentDashboardOffcanvasList item="my profile" params="/staff_dashboard/profile" icons="fas fa-user" />
+              <StudentDashboardOffcanvasList item="change password" params="/staff_dashboard/change_password" icons="fas fa-lock" />
+              <StudentDashboardOffcanvasList item="edit details" params="/staff_dashboard/edit_details" icons="fas fa-edit" />
+            </div>
+            <div className="grid gap-y-5">
+              <StudentDashboardOffcanvasTitle title="academics" />
+              <StudentDashboardOffcanvasList item="course registration" icons="fas fa-user" />
+              <StudentDashboardOffcanvasList item="registration history" icons="fas fa-lock" />
+              <StudentDashboardOffcanvasList item="results" icons="fas fa-edit" />
+            </div>
+            <div className="grid gap-y-5">
+              <StudentDashboardOffcanvasTitle title="payment" />
+              <StudentDashboardOffcanvasList item="pay tuition" icons="fas fa-user" />
+              <StudentDashboardOffcanvasList item="payment history" icons="fas fa-lock" />
+              <StudentDashboardOffcanvasList item="results" icons="fas fa-edit" />
+            </div>
+            <div className="grid gap-y-5">
+              <StudentDashboardOffcanvasTitle title="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/studentDashboardComponents/StudentDashboardOffcanvasList.jsx
+++ src/components/studentDashboardComponents/StudentDashboardOffcanvasList.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const StudentDashboardOffcanvasList = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(params)
+  }
   return (
-    <div>StudentDashboardOffcanvasList</div>
+    <>
+    <div className="grid gap-y-3" onClick={()=> handleClick()} style={{cursor: "pointer"}}>
+        <div className="flex gap-5">
+          <i className={icons} style={{marginTop: 'auto', marginBottom: 'auto'}}></i>
+          <div className="capitalize">{item}</div>
+        </div>
+      </div>
+    </>
   )
 }
 
