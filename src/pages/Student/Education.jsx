7467be95276e4d9843b7674d642b0364eae4d3ba
Adeolu Amole <adeoluamole@gmail.com>
2023-06-10 17:01:10 +0100

    Working on the student sign up page

--- src/pages/Student/Education.jsx
+++ src/pages/Student/Education.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import "./Education.css";
 
 const Education = () => {
   return (
-    <div>Education</div>
-  )
-}
+    <>
+      <div className="education-form row g-3">
+        <div className="education-input-container ic1">
+          <input
+            placeholder=""
+            type="text"
+            className="education-input"
+            id="firstname"
+          />
+          <div className="education-cut"></div>
+          <label className="education-iLabel" for="firstname">
+            First name
+          </label>
+        </div>
 
-export default Education
+        <div className="education-input-container ic2">
+          <input
+            placeholder=""
+            type="text"
+            className="education-input"
+            id="lastname"
+          />
+          <div className="education-cut"></div>
+          <label className="education-iLabel" for="lastname">
+            Last name
+          </label>
+        </div>
+        <div className="education-input-container ic2">
+          <input
+            placeholder=""
+            type="text"
+            className="education-input"
+            id="email"
+          />
+          <div className="education-cut cut-short"></div>
+          <label className="education-iLabel" for="email">
+            Email
+          </label>
+        </div>
+        <button className="education-submit" type="text">
+          submit
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Education;
