b44cad504a2aa8898645b6f89fc37988916d310c
Adeolu Amole <adeoluamole@gmail.com>
2023-06-10 18:23:17 +0100

    Working on the student sign up page

--- src/pages/Student/Education.jsx
+++ src/pages/Student/Education.jsx
@@ -6,14 +6,19 @@ const Education = () => {
     <>
       <div class="education-login-box">
         <form className="education-form">
-          <div class="education-user-box">
+          <div class="education-input-box">
+            <input required="required" className="education-input" type="text" />
+            <span>Username</span>
+            <i></i>
+          </div>
+          {/* <div class="education-user-box">
             <input required="" name="" className="education-input" type="text" />
             <label className="education-label">Email</label>
           </div>
           <div class="education-user-box">
             <input required="" name="" className="education-input" type="password" />
             <label className="education-label">Password</label>
-          </div>
+          </div> */}
           <a href="#">
             <span></span>
             <span></span>
