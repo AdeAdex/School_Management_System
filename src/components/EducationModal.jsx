25a49324a3a250d2ffa2d59e43031960e74e29c8
Adeolu Amole <adeoluamole@gmail.com>
2023-06-11 17:33:00 +0100

    Working on the student sign up page

--- src/components/EducationModal.jsx
+++ src/components/EducationModal.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import { useFormik } from "formik";
+import axios from "axios";
 
 const EducationModal = ({ isOpen, onClose }) => {
   let formik = useFormik({
@@ -15,8 +16,13 @@ const EducationModal = ({ isOpen, onClose }) => {
     },
 
     onSubmit: (values) => {
-        console.log(values);
-    }
+      console.log(values);
+      let endpoint = "http://localhost:2000/student_account/student_education";
+      axios.post(endpoint, values)
+      .then((response) => {
+        console.log(response);
+      });
+    },
   });
 
   return (
@@ -28,9 +34,14 @@ const EducationModal = ({ isOpen, onClose }) => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body className="text-uppercase">
-          <div class="education-login-box">
-            <form className="education-form" action="" method="post" onSubmit={formik.handleSubmit}>
-              <div class="education-input-box">
+          <div className="education-login-box">
+            <form
+              className="education-form"
+              action=""
+              method="post"
+              onSubmit={formik.handleSubmit}
+            >
+              <div className="education-input-box">
                 <input
                   required="required"
                   className="education-input"
@@ -41,63 +52,68 @@ const EducationModal = ({ isOpen, onClose }) => {
                 <span>exam</span>
                 <i></i>
               </div>
-              <div class="education-input-box">
+              <div className="education-input-box">
                 <input
                   required="required"
                   className="education-input"
                   type="text"
                   name="subject"
+                  onChange={formik.handleChange}
                 />
                 <span>subject</span>
                 <i></i>
               </div>
-              <div class="education-input-box">
+              <div className="education-input-box">
                 <input
                   required="required"
                   className="education-input"
                   type="text"
                   name="grade"
+                  onChange={formik.handleChange}
                 />
                 <span>grade</span>
                 <i></i>
               </div>
-              <div class="education-input-box">
+              <div className="education-input-box">
                 <input
                   required="required"
                   className="education-input"
                   type="text"
                   name="year"
+                  onChange={formik.handleChange}
                 />
                 <span>year</span>
                 <i></i>
               </div>
-              <div class="education-input-box">
+              <div className="education-input-box">
                 <input
                   required="required"
                   className="education-input"
                   type="text"
                   name="examNo"
+                  onChange={formik.handleChange}
                 />
                 <span>exam no</span>
                 <i></i>
               </div>
-              <div class="education-input-box">
+              <div className="education-input-box">
                 <input
                   required="required"
                   className="education-input"
                   type="text"
                   name="candidateNo"
+                  onChange={formik.handleChange}
                 />
                 <span>candidate number</span>
                 <i></i>
               </div>
-              <a className="submit-btn bg-primary" type="submit" href="#">
+              <button className="submit-btn bg-primary" type="submit" href="#">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
                 Submit
-              </a>
+              </button>
             </form>
           </div>
         </Modal.Body>
