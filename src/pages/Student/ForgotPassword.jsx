0c63c0b9d25a6dd6529314154dabc61b21acc4cb
Adeolu Amole <adeoluamole@gmail.com>
2023-06-20 15:13:28 +0100

    Working on the student education page

--- src/pages/Student/ForgotPassword.jsx
+++ src/pages/Student/ForgotPassword.jsx
@@ -2,6 +2,7 @@ import { useFormik } from "formik";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
+import '../Student/ForgotPassword.css'
 
 const ForgotPassword = () => {
   let location = useLocation();
@@ -16,11 +17,13 @@ const ForgotPassword = () => {
       const newValues = { ...values, myOTP };
       console.log(newValues);
       let endpoint = "http://localhost:2000/student_account/forgot_password";
-      axios
-      .post(endpoint, newValues)
-      .then((response) => {
+      axios.post(endpoint, newValues).then((response) => {
         // console.log(response.data.response[0]);
-        console.log(response.data.message);
+        if (response.data.status) {
+          console.log(response.data.message);
+        } else {
+          console.log(response.data.message);
+        }
       });
     },
   });
@@ -29,6 +32,27 @@ const ForgotPassword = () => {
       <form action="" method="post" onSubmit={formik.handleSubmit}>
         <input type="email" name="email" id="" onChange={formik.handleChange} />
         <button type="submit">Submit</button>
+      </form>
+
+      <form className="otp-verification-form">
+        <span className="otp-verification-close">X</span>
+
+        <div className="otp-verification-info">
+          <span className="otp-verification-title">OTP Verification</span>
+          <p className="otp-verification-description">Please enter the code we have sent you. </p>
+        </div>
+        <div className="otp-verification-inputs">
+          <input placeholder="" type="tel" maxLength="1" />
+          <input placeholder="" type="tel" maxLength="1" />
+          <input placeholder="" type="tel" maxLength="1" />
+          <input placeholder="" type="tel" maxLength="1" />
+        </div>
+        <a className="otp-verification-validate" href="#">
+          Verify
+        </a>
+        <p className="otp-verification-resend">
+          You don't receive the code ?<a className="otp-verification-resend-action">resend</a>
+        </p>
       </form>
     </>
   );
