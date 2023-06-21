862c8b9663e37f1e35d85913d40e4ed49f871dd2
Adeolu Amole <adeoluamole@gmail.com>
2023-06-21 22:58:31 +0100

    Working on the student education page

--- src/components/StudentForgotPasswordComponents/OTPVerification.jsx
+++ src/components/StudentForgotPasswordComponents/OTPVerification.jsx
@@ -1,107 +1,117 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import ChangePasswordForm from "./ChangePasswordForm";
 
-
-const OTPVerification = ({ isOpen, onClose, myOTP}) => {
+const OTPVerification = ({ isOpen, onClose, myOTP }) => {
   const [OTPInput, setOTPInput] = useState([0, 0, 0, 0]);
   const [myEmail, setMyEmail] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
 
-
-
   function handleSubmit(e) {
-        console.log(myOTP);
+    console.log(myOTP);
     console.log(myOTP);
     if (Number(OTPInput.join("")) === myOTP) {
-        setModalOpen(true);
+      setModalOpen(true);
     } else {
-        console.log('nooo');
+      console.log("nooo");
     }
     e.preventDefault();
   }
+
+  const input1Ref = useRef(null);
+  const input2Ref = useRef(null);
+  const input3Ref = useRef(null);
+  const input4Ref = useRef(null);
+
+  const handleChangeInput1 = (e) => {
+    setOTPInput([e.target.value, OTPInput[1], OTPInput[2], OTPInput[3]]);
+
+    if (e.target.value != "") {
+      input2Ref.current.focus();
+    }
+  };
+
+  const handleChangeInput2 = (e) => {
+    setOTPInput([OTPInput[0], e.target.value, OTPInput[2], OTPInput[3]]);
+
+    if (e.target.value != "") {
+      input3Ref.current.focus();
+    }
+  };
+
+  const handleChangeInput3 = (e) => {
+    setOTPInput([OTPInput[0], OTPInput[1], e.target.value, OTPInput[3]]);
+
+    if (e.target.value != "") {
+      input4Ref.current.focus();
+    }
+  };
+
   return (
     <>
-    <Modal show={isOpen} onHide={onClose} >
-    <Modal.Body className="text-uppercase">
-    <form className="otp-verification-form" onSubmit={handleSubmit}>
-        <span className="otp-verification-close">X</span>
+      <Modal show={isOpen} onHide={onClose}>
+        <Modal.Body className="text-uppercase">
+          <form className="otp-verification-form" onSubmit={handleSubmit}>
+            <span className="otp-verification-close">X</span>
 
-        <div className="otp-verification-info">
-          <span className="otp-verification-title">OTP Verification</span>
-          <p className="otp-verification-description">
-            Please enter the code we just sent to. {/* {myEmail} */}
-          </p>
-        </div>
-        <div className="otp-verification-inputs">
-          <input
-            placeholder=""
-            type="tel"
-            maxLength="1"
-            name="otpValue1"
-            onChange={(e) =>
-              setOTPInput([
-                e.target.value,
-                OTPInput[1],
-                OTPInput[2],
-                OTPInput[3],
-              ])
-            }
-          />
-          <input
-            placeholder=""
-            type="tel"
-            maxLength="1"
-            name="otpValue2"
-            onChange={(e) =>
-              setOTPInput([
-                OTPInput[0],
-                e.target.value,
-                OTPInput[2],
-                OTPInput[3],
-              ])
-            }
-          />
-          <input
-            placeholder=""
-            type="tel"
-            maxLength="1"
-            name="otpValue3"
-            onChange={(e) =>
-              setOTPInput([
-                OTPInput[0],
-                OTPInput[1],
-                e.target.value,
-                OTPInput[3],
-              ])
-            }
-          />
-          <input
-            placeholder=""
-            type="tel"
-            maxLength="1"
-            name="otpValue4"
-            onChange={(e) =>
-              setOTPInput([
-                OTPInput[0],
-                OTPInput[1],
-                OTPInput[2],
-                e.target.value,
-              ])
-            }
-          />
-        </div>
-        <button className="otp-verification-validate" type="submit">
-          Verify
-        </button>
-        <p className="otp-verification-resend">
-          You don't receive the code ?
-          <a className="otp-verification-resend-action">resend</a>
-        </p>
-      </form>
-    </Modal.Body>
-    </Modal>
-     <ChangePasswordForm isOpen={modalOpen}/>
+            <div className="otp-verification-info">
+              <span className="otp-verification-title">OTP Verification</span>
+              <p className="otp-verification-description">
+                Please enter the code we just sent to. {/* {myEmail} */}
+              </p>
+            </div>
+            <div className="otp-verification-inputs">
+              <input
+                placeholder=""
+                type="tel"
+                maxLength="1"
+                name="otpValue1"
+                ref={input1Ref}
+                onChange={handleChangeInput1}
+              />
+              <input
+                placeholder=""
+                type="tel"
+                maxLength="1"
+                name="otpValue2"
+                ref={input2Ref}
+                onChange={handleChangeInput2}
+              />
+              <input
+                placeholder=""
+                type="tel"
+                maxLength="1"
+                name="otpValue3"
+                ref={input3Ref}
+                onChange={handleChangeInput3}
+              />
+              <input
+                placeholder=""
+                type="tel"
+                maxLength="1"
+                name="otpValue4"
+                ref={input4Ref}
+                onChange={(e) =>
+                  setOTPInput([
+                    OTPInput[0],
+                    OTPInput[1],
+                    OTPInput[2],
+                    e.target.value,
+                  ])
+                }
+              />
+            </div>
+            <button className="otp-verification-validate" type="submit">
+              Verify
+            </button>
+            <p className="otp-verification-resend">
+              You don't receive the code ?
+              <a className="otp-verification-resend-action">resend</a>
+            </p>
+          </form>
+        </Modal.Body>
+      </Modal>
+      <ChangePasswordForm isOpen={modalOpen} />
     </>
   );
 };
