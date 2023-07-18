f746ce459bde29578ac794f1b403895302c61144
Adeolu Amole <adeoluamole@gmail.com>
2023-07-18 11:09:42 +0100

    Working on the student course registration page

--- src/pages/Staff/StaffEditDetails.jsx
+++ src/pages/Staff/StaffEditDetails.jsx
@@ -78,10 +78,27 @@ const StaffEditDetails = () => {
       console.log(newValues);
       let endpoint =
         "http://localhost:2000/staff_account/edit_details";
-      axios.post(endpoint, newValues);
-      // .then((response) => {
-      //   console.log(response.data.response.staffArray);
-      // });
+      axios.post(endpoint, newValues)
+      .then((response) => {
+        if (response.data.status) {
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top",
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
+
+          Toast.fire({
+            icon: "success",
+            title: response.data.message,
+          });
+        }
+      });
     },
   });
 
