b5a014636dc23cad065ac88be902ed72b24ba96e
Amole Adeolu <98040608+Adex2210@users.noreply.github.com>
2023-07-21 05:52:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from 'tailwindcss';
 // const dotenv = require('dotenv');
 // const dotenvExpand = require('dotenv-expand');
 
@@ -9,7 +10,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
