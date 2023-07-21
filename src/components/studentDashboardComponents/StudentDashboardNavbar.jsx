b091fb00dd0ed309901727117189559431de0bec
Amole Adeolu <98040608+Adex2210@users.noreply.github.com>
2023-07-21 10:08:39 +0100

    Update StudentDashboardNavbar.jsx

--- src/components/studentDashboardComponents/StudentDashboardNavbar.jsx
+++ src/components/studentDashboardComponents/StudentDashboardNavbar.jsx
@@ -64,7 +64,7 @@ const StudentDashboardNavbar = () => {
   return (
     <>
       <div
-        className="shadow d-flex tailwind-page"
+        className="shadow d-flex"
         id="nav"
         style={{ width: "100%", height: "80px" }}
       >
