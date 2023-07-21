5fc42a3f9fa16a575ae4d343def643507df7007c
Amole Adeolu <98040608+Adex2210@users.noreply.github.com>
2023-07-21 13:03:16 +0100

    Update StudentPortalDashboard.jsx

--- src/pages/Student/StudentPortalDashboard.jsx
+++ src/pages/Student/StudentPortalDashboard.jsx
@@ -181,7 +181,7 @@ const StudentPortalDashboard = () => {
     <>
       <section
         id=""
-        className="d-flex tailwind-page"
+        className="flex"
         style={{ width: "100%", height: "100vh", overflow: "hidden" }}
       >
         <div
