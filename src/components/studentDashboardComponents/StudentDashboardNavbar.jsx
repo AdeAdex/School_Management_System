9f6e35d9a67c8aa8e8526859418048e2f2e45af3
Adeolu Amole <adeoluamole@gmail.com>
2023-07-22 21:18:14 +0100

    Working on the student forgot password page

--- src/components/studentDashboardComponents/StudentDashboardNavbar.jsx
+++ src/components/studentDashboardComponents/StudentDashboardNavbar.jsx
@@ -8,27 +8,7 @@ import AvatarUploader from "./AvatarUploader";
 
 const StudentDashboardNavbar = () => {
   const globalState = useSelector((state) => state.portalReducer.studentInfo);
-  // const offCanvas = () => {
-  //   toggle();
-  //   if (offCan.style.width == "20%") {
-  //     offCan.style.width = "5%";
-  //     nav.style.width = "95%";
-  //     menu.style.setProperty("display", "none", "important");
-  //   } else {
-  //     offCan.style.width = "20%";
-  //     nav.style.width = "80%";
-  //     menu.style.setProperty("display", "block", "important");
-  //   }
-
-  //   var x = window.matchMedia("(max-width: 768px)");
-
-  //   if (x.matches) {
-  //     ourBody.classList.add("new-class");
-  //   } else {
-  //   }
-  // };
-
-
+ 
   const offCanvas = () => {
     const offCan = document.getElementById("offCan");
     const nav = document.getElementById("nav");
