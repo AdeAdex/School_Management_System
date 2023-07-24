035730706835038ab99799131ad55a503ddf95aa
Adeolu Amole <adeoluamole@gmail.com>
2023-07-24 17:21:58 +0100

    Working on the student forgot password page

--- src/pages/Staff/SignUp.jsx
+++ src/pages/Staff/SignUp.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./SignUp.css";
-import SignupHero from "../../components/StudentSignupComponents/SignupHero";
-import SignupForm from "../../components/StudentSignupComponents/SignupForm";
+// import SignupHero from "../../components/StudentSignupComponents/SignupHero";
+// import SignupForm from "../../components/StudentSignupComponents/SignupForm";
 
 const SignUp = () => {
   return (
     <>
       <section
         className="signup-container d-flex flex-lg-row flex-md-column flex-sm-column  mx-auto py-3 gap-3"
-        style={{ width: "75%", height: "100%" }}
+        style={{ width: "75%", height: "100vh" }}
       >
         <SignupHero />
         <SignupForm />
