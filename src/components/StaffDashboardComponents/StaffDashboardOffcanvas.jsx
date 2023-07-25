df1007e1caa1ce370fb8608e3a6556ffd60711f9
Adeolu Amole <adeoluamole@gmail.com>
2023-07-25 17:02:22 +0100

    Working on the satff dashboard page

--- src/components/StaffDashboardComponents/StaffDashboardOffcanvas.jsx
+++ src/components/StaffDashboardComponents/StaffDashboardOffcanvas.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import StaffDashboardOffcanvasTitle from './StaffDashboardOffcanvasTitle'
+import StaffDashboardOffcanvasList from './StaffDashboardOffcanvasList'
+import StaffDashboardOffcanvasList2 from './StaffDashboardOffcanvasList2'
+
 
 const StaffDashboardOffcanvas = () => {
+  const offcanvasState = useSelector((state) => state.portalReducer.hide_show)
+
   return (
     <>
-      
+      <div
+        className="position-relativ"
+        id=""
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div className="dashboard-offcanvas w-100 position-relativ text-white">
+          <div
+            className="w-100 d-flex justify-content-center shadow"
+            style={{ height: "80px", width: "100%", borderBottom: '2px solid white' }}
+          >
+            <img
+              src="/pic/ade.png"
+              alt=""
+              className="my-auto"
+              style={{ width: "60px", height: "60px" }}
+            />
+          </div>
+          {!offcanvasState &&
+          <div
+            className="text-capitalize text-center py-5 fs-5 fw-bold"
+            id="menu"
+          >
+            personal menu
+          </div>}
+          <div className={`px-4 d-grid gap-5 ${offcanvasState ? 'meme' : ''}`} >
+            <div className="d-grid gap-3">
+            {!offcanvasState && <StaffDashboardOffcanvasTitle title="main menu" /> }
+              <StaffDashboardOffcanvasList item="dashboard" params="/student_dashboard/home" label="dashboard" icons="fas fa-border-all" />
+            </div>
+            <div className="d-grid gap-3">
+               {!offcanvasState && <StaffDashboardOffcanvasTitle title="profile" /> }
+              <StaffDashboardOffcanvasList item="my profile" params="/student_dashboard/profile" label="my profile" icons="fas fa-user" />
+              <StaffDashboardOffcanvasList item="change password" params="/student_dashboard/change_password" label="change password" icons="fas fa-lock" />
+              <StaffDashboardOffcanvasList item="edit details" params="/student_dashboard/edit_details" label="edit details" icons="fas fa-edit" />
+            </div>
+            <div className="d-grid gap-3">
+               {!offcanvasState && <StaffDashboardOffcanvasTitle title="academics" /> }
+              <StaffDashboardOffcanvasList item="resources" params="/student_dashboard/resources" label="resources" icons="fas fa-file"/>
+              <StaffDashboardOffcanvasList item="course registration" params="/student_dashboard/course_registration" label="course registration" icons="fas fa-user" />
+              <StaffDashboardOffcanvasList item="registration history" label="registration history" icons="fas fa-lock" />
+              <StaffDashboardOffcanvasList item="results" label="results" icons="fas fa-edit" />
+            </div>
+            <div className="d-grid gap-3">
+               {!offcanvasState && <StaffDashboardOffcanvasTitle title="payment" /> }
+              <StaffDashboardOffcanvasList item="pay tuition" label="pay tuition" icons="fas fa-user" />
+              <StaffDashboardOffcanvasList item="payment history" label="payment history" icons="fas fa-lock" />
+            </div>
+            <div className="d-grid gap-3 mb-5">
+             
+              <StaffDashboardOffcanvasList2 item="logout" label="logout" icons="fas fa-user"/>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
