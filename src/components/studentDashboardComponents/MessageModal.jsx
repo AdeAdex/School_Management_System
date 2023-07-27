9eaad2d5ad053421c426e29610b46df277168fcb
Adeolu Amole <adeoluamole@gmail.com>
2023-07-27 22:32:58 +0100

    Working on the student portal page

--- src/components/studentDashboardComponents/MessageModal.jsx
+++ src/components/studentDashboardComponents/MessageModal.jsx
@@ -1,28 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Group, Button, ScrollArea } from "@mantine/core";
+// import { Modal, Group, Button, ScrollArea } from "@mantine/core";
+import '../studentDashboardComponents/MessageModal.css'
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute',
+  top: '85px',
+  left: '',
+  right: '40px',
+  // transform: 'translate(-50%, -50%)',
+  width: 400,
+  height: '87%',
+  bgcolor: 'background.paper',
+  // border: '2px solid #000',
+  boxShadow: 24,
+  padding: '10px',
+};
 
 const MessageModal = ({ opened, onClose, myMessages }) => {
-  const [noTransitionOpened, setNoTransitionOpened] = useState(false);
-  const [slowTransitionOpened, setSlowTransitionOpened] = useState(false);
-
   //   useEffect(() => {
   // //     console.log(myMessages);
   //   }, [myMessages])
+ 
 
   return (
     <>
-      <Modal
+      {/* <Modal
         opened={opened}
         onClose={onClose}
         title="Messages"
         scrollAreaComponent={ScrollArea.Autosize}
         transitionProps={{ transition: "rotate-left" }}
-        // xOffset={0}
+        // size="70%"
+        xOffset="250px"
         position="right"
+        style={{position: 'absolute'}}
+        
       >
         {myMessages.map((message) => (
-          <div className="d-flex w-100" key={message._id}>
+          <div className="d-flex w-100 each-modal-message" key={message._id}>
             <img src="/pic/avatar.png" style={{ width: "80px" }} alt="" />
             <div className="my-auto">
               <div>{message.senderName}</div>
@@ -32,7 +54,43 @@ const MessageModal = ({ opened, onClose, myMessages }) => {
           </div>
         ))}
         
+      </Modal> */}
+
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={opened}
+        onClose={onClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={opened}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Messages
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+            {myMessages.map((message) => (
+          <div className="d-flex w-100 each-modal-message" key={message._id}>
+            <img src="/pic/avatar.png" style={{ width: "80px" }} alt="" />
+            <div className="my-auto">
+              <div>{message.senderName}</div>
+              <div>{message.messageSubject}</div>
+            </div>
+            <div className="ms-auto my-auto">{message.messageDate}</div>
+          </div>
+        ))}
+            </Typography>
+          </Box>
+        </Fade>
       </Modal>
+
     </>
   );
 };
