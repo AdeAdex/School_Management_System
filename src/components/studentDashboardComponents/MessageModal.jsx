a4ac01e8c2d23bedffe8f5f5fae0f222108cda53
Adeolu Amole <adeoluamole@gmail.com>
2023-07-27 22:52:13 +0100

    Working on the student portal page

--- src/components/studentDashboardComponents/MessageModal.jsx
+++ src/components/studentDashboardComponents/MessageModal.jsx
@@ -30,7 +30,7 @@ const MessageModal = ({ opened, onClose, myMessages }) => {
 
 
   const openChatModal = () => {
-    onClose={close}
+    onClose();
   }
 
   return (
@@ -99,37 +99,37 @@ const MessageModal = ({ opened, onClose, myMessages }) => {
       </Modal>
 
       <div
-        class="modal fade"
+        className="modal fade"
         id="staticBackdrop"
-        data-bs-backdrop="static"
+        data-bs-backdrop="false"
         data-bs-keyboard="false"
-        tabindex="-1"
+        tabIndex="-1"
         aria-labelledby="staticBackdropLabel"
         aria-hidden="true"
       >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="staticBackdropLabel">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="staticBackdropLabel">
                 Modal title
               </h1>
               <button
                 type="button"
-                class="btn-close"
+                className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div class="modal-body">...</div>
-            <div class="modal-footer">
+            <div className="modal-body">...</div>
+            <div className="modal-footer">
               <button
                 type="button"
-                class="btn btn-secondary"
+                className="btn btn-secondary"
                 data-bs-dismiss="modal"
               >
                 Close
               </button>
-              <button type="button" class="btn btn-primary">
+              <button type="button" className="btn btn-primary">
                 Understood
               </button>
             </div>
