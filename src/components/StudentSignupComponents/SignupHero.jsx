8a708d6ebe15b00c3db1ad3235a0a0021d77d7f2
Adeolu Amole <adeoluamole@gmail.com>
2023-07-27 04:55:57 +0100

    Working on the student login page

--- src/components/StudentSignupComponents/SignupHero.jsx
+++ src/components/StudentSignupComponents/SignupHero.jsx
@@ -8,8 +8,8 @@ const SignupHero = ({ isAdmissionPage }) => {
 
   return (
     <>
-    {isAdmissionPage ? (
-      <div
+      {isAdmissionPage ? (
+        <div
           className="h-100 py-4 px-5 d-flex flex-column position-relative text-white signup-header justify-content-center align-items-center"
           style={{
             width: "30%",
@@ -20,83 +20,88 @@ const SignupHero = ({ isAdmissionPage }) => {
             backgroundRepeat: "no-repeat",
           }}
         >
-          <div className="d-flex flex-column justify-content-center">
+          <div className="d-flex flex-column justify-content-center align-items-center">
             <img
               src="/pic/ade.png"
               alt=""
               className=""
               style={{ width: "100px" }}
             />
-            <div className="">Adex School</div>
-            <div className="">Admission Portal</div>
+            <div>
+              <div className="fw-bold fs-4" style={{ fontFamily: "" }}>
+                Adex School
+              </div>
+              <div className="fs-6 mt-3" style={{ fontFamily: "georgia" }}>
+                Admission Portal
+              </div>
+            </div>
           </div>
         </div>
-    ) : (
-      <div
-        className="h-100 py-4 px-5 d-flex flex-column position-relative text-white signup-header"
-        style={{
-          width: "30%",
-          backgroundColor: "#3c37ff",
-          borderRadius: "10px",
-          height: "100%",
-          backgroundImage: "url('/pic/signupimg.jpg')",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        {isLargeScreen ? (
-          <Link to="/" style={{ color: "white" }}>
-            <i
-              className="fas fa-circle-chevron-left"
-              style={{ fontSize: "30px" }}
-            ></i>
-          </Link>
-        ) : null}
+      ) : (
+        <div
+          className="h-100 py-4 px-5 d-flex flex-column position-relative text-white signup-header"
+          style={{
+            width: "30%",
+            backgroundColor: "#3c37ff",
+            borderRadius: "10px",
+            height: "100%",
+            backgroundImage: "url('/pic/signupimg.jpg')",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          {isLargeScreen ? (
+            <Link to="/" style={{ color: "white" }}>
+              <i
+                className="fas fa-circle-chevron-left"
+                style={{ fontSize: "30px" }}
+              ></i>
+            </Link>
+          ) : null}
 
-        <div className="d-flex justify-content-center w-100">
-          <img
-            src="pic/ade.png"
-            className="mt-5 mb-lg-1 d-flex align-items-center"
-            style={{ width: "50px" }}
-            alt=""
-          />
-        </div>
+          <div className="d-flex justify-content-center w-100">
+            <img
+              src="pic/ade.png"
+              className="mt-5 mb-lg-1 d-flex align-items-center"
+              style={{ width: "50px" }}
+              alt=""
+            />
+          </div>
 
-        {isLargeScreen ? (
-          <>
-            <h2
-              className="mt-lg-1 header-text"
-              style={{ textTransform: "capitalize" }}
-            >
-              start your <br />
-              journey with us.
-            </h2>
+          {isLargeScreen ? (
+            <>
+              <h2
+                className="mt-lg-1 header-text"
+                style={{ textTransform: "capitalize" }}
+              >
+                start your <br />
+                journey with us.
+              </h2>
+              <div>Discover the world best education</div>
+            </>
+          ) : (
             <div>Discover the world best education</div>
-          </>
-        ) : (
-          <div>Discover the world best education</div>
-        )}
+          )}
 
-        {isLargeScreen ? (
-          <SignupCarousel
-            carousel_content="This is a very nice school for any child who want the best
+          {isLargeScreen ? (
+            <SignupCarousel
+              carousel_content="This is a very nice school for any child who want the best
                 education. I really recommend them 💯"
-            carousel_content2="The service render at Adex School is simply unbelievable. I'm
+              carousel_content2="The service render at Adex School is simply unbelievable. I'm
                 really satisfied with the way my daughter is improving in her
                 study"
-            carousel_content3="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              carousel_content3="Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Dolore, minima quia eligendi repellat eius in molestias
                 distinctio, expedita labore amet"
-            carousel_name1="Grace George"
-            carousel_name2="Timson K"
-            carousel_name3="Adeolu Adex"
-            carousel_role1="Fashionist"
-            carousel_role2="Freelancer"
-            carousel_role3="Developer"
-          />
-        ) : null}
-      </div>
-    )}
-      
+              carousel_name1="Grace George"
+              carousel_name2="Timson K"
+              carousel_name3="Adeolu Adex"
+              carousel_role1="Fashionist"
+              carousel_role2="Freelancer"
+              carousel_role3="Developer"
+            />
+          ) : null}
+        </div>
+      )}
     </>
   );
 };
