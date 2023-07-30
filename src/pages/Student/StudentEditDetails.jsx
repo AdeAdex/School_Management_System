c11df8d2cf49f4f2314f0a9fe206f3df73e06829
Adeolu Amole <adeoluamole@gmail.com>
2023-07-30 17:51:13 +0100

    Working on the chat app page

--- src/pages/Student/StudentEditDetails.jsx
+++ src/pages/Student/StudentEditDetails.jsx
@@ -1,90 +1,90 @@
-// import axios from "axios";
-// import { useFormik } from "formik";
-// import React, { useEffect, useState } from "react";
-// import { useSelector } from "react-redux";
-// import { BsFillEmojiSmileFill } from "react-icons/bs";
-// import { Textarea } from "@mantine/core";
-// import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useFormik } from "formik";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { BsFillEmojiSmileFill } from "react-icons/bs";
+import { Textarea } from "@mantine/core";
+import { useLocation } from "react-router-dom";
 
 
-// const StudentEditDetails = ({ socket }) => {
-//   const location = useLocation();
-//   const id = new URLSearchParams(location.search).get("id");
-//   const name = new URLSearchParams(location.search).get("name");
-//   // const picture = new URLSearchParams(location.search).get("picture");
+const StudentEditDetails = ({ socket }) => {
+  // const location = useLocation();
+  // const id = new URLSearchParams(location.search).get("id");
+  // const name = new URLSearchParams(location.search).get("name");
+  // // const picture = new URLSearchParams(location.search).get("picture");
 
-//   useEffect(() => {
-//     if (socket.current) {
-//       socket.current.on("brodcastMsg", (receivedMessage) => {
-//         console.log(receivedMessage);
-//       });
-//     }
-//   });
+  // useEffect(() => {
+  //   if (socket.current) {
+  //     socket.current.on("brodcastMsg", (receivedMessage) => {
+  //       console.log(receivedMessage);
+  //     });
+  //   }
+  // });
 
-//   const [message, setMessage] = useState("");
-//   const [allmessages, setAllmessages] = useState([]);
+  // const [message, setMessage] = useState("");
+  // const [allmessages, setAllmessages] = useState([]);
 
-//   const sendMessage = () => {
-//     let payload = {
-//       message: message,
-//       name: name,
-//       messageDate: new Date().toLocaleDateString("en-GB", {
-//         year: "2-digit",
-//         month: "2-digit",
-//         day: "2-digit",
-//       }),
-//       messageTime: new Date().toLocaleTimeString("en-US", {
-//         hour12: false,
-//         hour: "2-digit",
-//         minute: "2-digit",
-//       }),
-//       picture: localStorage.cloudImage,
-//       id: id,
-//     };
+  // const sendMessage = () => {
+  //   let payload = {
+  //     message: message,
+  //     name: name,
+  //     messageDate: new Date().toLocaleDateString("en-GB", {
+  //       year: "2-digit",
+  //       month: "2-digit",
+  //       day: "2-digit",
+  //     }),
+  //     messageTime: new Date().toLocaleTimeString("en-US", {
+  //       hour12: false,
+  //       hour: "2-digit",
+  //       minute: "2-digit",
+  //     }),
+  //     picture: localStorage.cloudImage,
+  //     id: id,
+  //   };
 
-//     // console.log(payload);
-//     socket.current.emit("sentMsg", payload);
-//     setAllmessages([...allmessages, payload]);
-//   };
+  //   // console.log(payload);
+  //   socket.current.emit("sentMsg", payload);
+  //   setAllmessages([...allmessages, payload]);
+  // };
 
-//   return (
-//     <>
-//       <div>
-//         {allmessages.map((msg, index) => (
-//           <div key={index}>
-//             <div>{msg.name}</div>
-//             <div>{msg.message}</div>
-//             <div>{msg.messageDate}</div>
-//             <div>{msg.messageTime}</div>
-//             <img
-//               src={msg.picture}
-//               alt=""
-//               style={{ width: "50px", borderRadius: "50%" }}
-//             />
-//           </div>
-//         ))}
-//       </div>
-//       <div className="modal-footer d-flex w-100">
-//         <BsFillEmojiSmileFill size={20} color="orange" />
-//         <Textarea
-//           label=""
-//           placeholder="Message"
-//           autosize
-//           minRows={1}
-//           maxRows={4}
-//           style={{ width: "70%", backgroundColor: "" }}
-//           onChange={(e) => setMessage(e.target.value)}
-//         />
-//         <button
-//           type="submit"
-//           onClick={sendMessage}
-//           className="btn btn-sm btn-primary"
-//         >
-//           Send
-//         </button>
-//       </div>
-//     </>
-//   );
-// };
+  return (
+    <>
+      {/* <div>
+        {allmessages.map((msg, index) => (
+          <div key={index}>
+            <div>{msg.name}</div>
+            <div>{msg.message}</div>
+            <div>{msg.messageDate}</div>
+            <div>{msg.messageTime}</div>
+            <img
+              src={msg.picture}
+              alt=""
+              style={{ width: "50px", borderRadius: "50%" }}
+            />
+          </div>
+        ))}
+      </div> */}
+      <div className="modal-footer d-flex w-100">
+        <BsFillEmojiSmileFill size={20} color="orange" />
+        <Textarea
+          label=""
+          placeholder="Message"
+          autosize
+          minRows={1}
+          maxRows={4}
+          style={{ width: "70%", backgroundColor: "" }}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          type="submit"
+          onClick={sendMessage}
+          className="btn btn-sm btn-primary"
+        >
+          Send
+        </button>
+      </div>
+    </>
+  );
+};
 
-// export default StudentEditDetails;
+export default StudentEditDetails;
