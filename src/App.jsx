6b4cad1a0cf7363757a7015be5183c5ced5d99d3
Amole Adeolu <98040608+Adex2210@users.noreply.github.com>
2023-08-02 05:32:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ import Chat from './components/studentDashboardComponents/Chat'
 
 function App() {
   let socketRef = useRef()
-  const endpoint = "https://school-portal-backend-adex2210.vercel.app"
+  const endpoint = "https://school-portal-backend-adex2210.vercel.app/"
   const globalState = useSelector((state)=>state.portalReducer.firstName)
   console.log(globalState);
   const [count, setCount] = useState(0)
