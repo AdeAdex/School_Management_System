1f2a3526cf128f1d04481a804aba254be73e9aa6
Adeolu Amole <adeoluamole@gmail.com>
2023-08-10 23:11:41 +0100

    Working on the the questions and answer page

--- src/pages/Student/Test.jsx
+++ src/pages/Student/Test.jsx
@@ -3,36 +3,28 @@ import axios from "axios";
 
 const Test = () => {
   const [questions, setQuestions] = useState([]);
-  //   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-//   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(
-//     localStorage.getItem("currentQuestionIndex") || 0
-//   );
-const [currentQuestionIndex, setCurrentQuestionIndex] = useState(
-        Number(localStorage.getItem("currentQuestionIndex")) || 0
-      );
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(
+    Number(localStorage.getItem("currentQuestionIndex")) || 0
+  );
   const [selectedOption, setSelectedOption] = useState("");
   const [showCorrectAnswer, setShowCorrectAnswer] = useState(false);
   const [scores, setScores] = useState(0);
   const [clicked, setClicked] = useState(false);
 
-//   useEffect(() => {
-//     let endpoint = "http://localhost:2000/staff_account/questions";
-//     axios.get(endpoint).then((response) => {
-//       setQuestions(response.data);
-//     });
-//     localStorage.setItem("currentQuestionIndex", String(currentQuestionIndex));
-//   }, [currentQuestionIndex]);
-
-useEffect(() => {
-        let endpoint = "https://school-portal-backend-adex2210.vercel.app/staff_account/questions";
-        axios.get(endpoint).then((response) => {
-          setQuestions(response.data);
-          // Update localStorage after fetching questions
-          if (localStorage.getItem("currentQuestionIndex") === null) {
-            localStorage.setItem("currentQuestionIndex", String(currentQuestionIndex));
-          }
-        });
-      }, []);
+  useEffect(() => {
+    let endpoint =
+      "https://school-portal-backend-adex2210.vercel.app/staff_account/questions";
+    axios.get(endpoint)
+    .then((response) => {
+      setQuestions(response.data);
+      if (localStorage.getItem("currentQuestionIndex") === null) {
+        localStorage.setItem(
+          "currentQuestionIndex",
+          String(currentQuestionIndex)
+        );
+      }
+    });
+  }, []);
 
   const handleNextClick = () => {
     if (currentQuestionIndex < questions.length - 1) {
