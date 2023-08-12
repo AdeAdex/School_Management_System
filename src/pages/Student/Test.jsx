c32ff73a3117759f4819f3ba2444cb388eb7b24b
Adeolu Amole <adeoluamole@gmail.com>
2023-08-12 08:35:58 +0100

    Working on the the questions and answer page

--- src/pages/Student/Test.jsx
+++ src/pages/Student/Test.jsx
@@ -168,7 +168,7 @@ const Test = () => {
                   <p className="my-3">{currentQuestion.content}</p>
                   <ul className="d-flex flex-column mx-auto">
                     {currentQuestion.options.map((option, index) => (
-                      <label key={index} className="d-flex align-items-center">
+                      <label key={index} className="d-flex align-items-center mx-auto">
                         <input
                           type="radio"
                           name="option"
