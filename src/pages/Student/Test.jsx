d2496f27e114848990db4967482a07830aa9e693
Adeolu Amole <adeoluamole@gmail.com>
2023-08-15 17:59:49 +0100

    Working on the the questions and answer page

--- src/pages/Student/Test.jsx
+++ src/pages/Student/Test.jsx
@@ -261,7 +261,7 @@ const Test = () => {
         setBeginExam(true);
         localStorage.setItem("examStarted", "true");
 
-        const countdownTime = 3000; // 5 minutes in seconds
+        const countdownTime = 300; // 5 minutes in seconds
         localStorage.setItem("countdownStartTime", Date.now());
         localStorage.setItem("countdownTimeRemaining", countdownTime);
 
