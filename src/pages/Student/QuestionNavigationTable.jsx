7d542b97d9f02a65bf37156202449d29951ace27
Adeolu Amole <adeoluamole@gmail.com>
2023-08-16 11:03:09 +0100

    Working on the the questions and answer page

--- src/pages/Student/QuestionNavigationTable.jsx
+++ src/pages/Student/QuestionNavigationTable.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const QuestionNavigationTable = ({
   totalQuestions,
   currentQuestionIndex,
@@ -13,72 +12,38 @@ const QuestionNavigationTable = ({
   );
   return (
     <>
-
-<div className="question-navigation-table">
-  <h4 className="question-h4">Questions:</h4>
-  <ul className="question-number-list">
-    {Array.from({ length: totalQuestions }, (_, index) => (
-      <li
-        key={index}
-        onClick={() => handleQuestionNavigation(index)}
-        className="question-number-item d-flex justify-content-center"
-      >
-        <span className='question-number'>
-          {index + 1}
-        </span>
-        {answeredQuestions.includes(index + 1) && <span className="tick-mark answered">&#10004;</span>}
-      </li>
-    ))}
-  </ul>
-</div>
-
-
-{/* <div className="question-navigation-table">
-  <h4 className="question-h4">Questions:</h4>
-  <ul className="question-number-list">
-    {Array.from({ length: totalQuestions }, (_, index) => (
-      <li
-        key={index}
-        onClick={() => handleQuestionNavigation(index)}
-        className="question-number-item"
-      >
-        <span className={`question-number ${answeredQuestions.includes(index + 1) ? "answered" : ""}`}>
-          {index + 1}
-        </span>
-        {answeredQuestions.includes(index + 1) && <span className="tick-mark">&#10004;</span>}
-      </li>
-    ))}
-  </ul>
-</div> */}
-
-      {/* <div className="question-navigation-table">
+      <div className="question-navigation-table">
         <h4 className="question-h4">Questions:</h4>
         <ul className="question-number-list">
-        {Array.from({ length: totalQuestions }, (_, index) => (
-          <li
-            key={index}
-            className={answeredQuestions.includes(index + 1) ? "answered" : ""}
-            onClick={() => handleQuestionNavigation(index)}
-          >
-            {index + 1}
-          </li>
-        ))}
+          {Array.from({ length: totalQuestions }, (_, index) => (
+            <li
+              key={index}
+              onClick={() => handleQuestionNavigation(index)}
+              className="question-number-item d-flex justify-content-center"
+            >
+              <span
+                className={`question-number ${
+                  answeredQuestions.includes(index + 1) &&
+                  window.innerWidth <= 768
+                    ? "green"
+                    : ""
+                }`}
+              >
+                {index + 1}
+              </span>
+              {answeredQuestions.includes(index + 1) &&
+                window.innerWidth > 768 && (
+                  <span
+                    className="tick-mark answered"
+                    style={{ position: "absolute", right: 40 }}
+                  >
+                    &#10004;
+                  </span>
+                )}
+            </li>
+          ))}
         </ul>
-      </div> */}
-
-      {/* <div className="question-navigation-table">
-      <ul>
-        {Array.from({ length: totalQuestions }, (_, index) => (
-          <li
-            key={index}
-            className={answeredQuestions.includes(index + 1) ? "answered" : ""}
-            onClick={() => handleQuestionNavigation(index)}
-          >
-            {index + 1}
-          </li>
-        ))}
-      </ul>
-    </div> */}
+      </div>
     </>
   );
 };
