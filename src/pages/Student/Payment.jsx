c9409a8e5b5b3188d1e3e5ab0615ad03c6d4eb9d
Adeolu Amole <adeoluamole@gmail.com>
2023-08-18 12:22:04 +0100

    Working on the the questions and answer page

--- src/pages/Student/Payment.jsx
+++ src/pages/Student/Payment.jsx
@@ -6,7 +6,7 @@ const Payment = ({paid}) => {
 
   return (
     <>
-      <div className="payment-card">
+      {/* <div className="payment-card">
       <div className="status">
         <div className={`status-value w-100 ${paid ? 'paid' : 'not-paid'}`}> <span>Application Fee: </span> 
           <small>{paid ? 'Paid' : 'Not Paid'}</small>
@@ -15,6 +15,24 @@ const Payment = ({paid}) => {
       <div className="amount"><span>Amount Paid:</span> <small className='fw-bold'>₦5000</small></div>
       <div className="method">Pay Method: 'card'</div>
       <div className="payment-slip"></div>
+    </div> */}
+
+    <div className="payment-card">
+      <div className="status">
+        <div className={`status-value w-100 ${paid ? 'paid' : 'not-paid'}`}>
+          <span>Application Fee: </span>
+          <small>{paid ? 'Paid' : 'Not Paid'}</small>
+        </div>
+      </div>
+      <div className="amount">
+        <span>Amount Paid:</span> <small className="fw-bold">₦5000</small>
+      </div>
+      <div className="method">
+        Pay Method: card
+      </div>
+      <div className="payment-slip">
+        <span>Payment Slip:</span> <small className="fw-bold">Upload your payment slip here</small>
+      </div>
     </div>
     </>
   )
