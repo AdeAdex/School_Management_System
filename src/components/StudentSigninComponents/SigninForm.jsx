6367ec6eb258226543d1b1b8e855e562f9f26f93
Adeolu Amole <adeoluamole@gmail.com>
2023-08-30 06:14:25 +0100

    Working on the the questions and answer page

--- src/components/StudentSigninComponents/SigninForm.jsx
+++ src/components/StudentSigninComponents/SigninForm.jsx
@@ -1,18 +1,22 @@
 import axios from "axios";
 import { useFormik } from "formik";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
-
+import { TypeAnimation } from "react-type-animation";
+import { useMediaQuery } from "react-responsive";
 
 const SigninForm = () => {
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
-  const [enteredEmail, setEnteredEmail] = useState("")
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [myMessage, setMyMessage] = useState("");
-
-
-
+  const [typingStatus, setTypingStatus] = useState("");
+  const [callbackText, setCallbackText] = useState("");
+  const [message, setMessage] = useState("");
+  const [text, setText] = useState("");
+  const [showTypingAnimation, setShowTypingAnimation] = useState(false);
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -23,35 +27,52 @@ const SigninForm = () => {
       email: "",
       password: "",
     },
-
     onSubmit: (values) => {
       setEnteredEmail(values.email);
       const endpoint = "http://localhost:2000/student_account/student_signin";
-      axios.post(endpoint, values)
-      .then((res) => {
+      axios.post(endpoint, values).then((res) => {
         if (res.data.status) {
           localStorage.studentSignInToken = res.data.studentSignInToken;
           navigate("/student_dashboard");
         } else {
-          setMyMessage(res.data.message)
+          setMyMessage(res.data.message);
           navigate("/student_signin");
-          console.log(res);
-          // const Toast = Swal.mixin({
-          //   toast: true,
-          //   position: "top",
-          //   showConfirmButton: false,
-          //   timer: 1500,
-          //   timerProgressBar: true,
-          //   didOpen: (toast) => {
-          //     toast.addEventListener("mouseenter", Swal.stopTimer);
-          //     toast.addEventListener("mouseleave", Swal.resumeTimer);
-          //   },
-          // });
+          if (res.data.errorMessage) {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top",
+              showConfirmButton: false,
+              timer: 1500,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+              },
+            });
 
-          // Toast.fire({
-          //   icon: "error",
-          //   title: res.data.message,
-          // });
+            Toast.fire({
+              icon: "error",
+              title: res.data.errorMessage,
+            });
+            setMessage("If you haven't made the payment for admission yet, you won't have a valid Student ID. Please make the payment to receive your Student ID.\n")
+          } else {
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "top",
+              showConfirmButton: false,
+              timer: 1500,
+              timerProgressBar: true,
+              didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer);
+                toast.addEventListener("mouseleave", Swal.resumeTimer);
+              },
+            });
+
+            Toast.fire({
+              icon: "error",
+              title: res.data.message,
+            });
+          }
         }
       });
     },
@@ -59,22 +80,77 @@ const SigninForm = () => {
 
 
   const navigateToOTP = () => {
-    navigate('/forgot_password', {state: {email: enteredEmail }});
-  }
+    navigate("/forgot_password", { state: { email: enteredEmail } });
+  };
 
 
   return (
     <>
       <div className="signin-form shadow bg-light">
+    
         <h1 className="fs-bolder">
+        {message && isSmallScreen ? (
+          <small style={{position: 'absolute', top: '10px', fontSize: '14px', fontWeight: 'unset'}}>
+          <TypeAnimation
+          sequence={[
+            1500,
+            () => {
+              setTypingStatus("Typing...");
+            },
+            message,
+            () => {
+              setTypingStatus("Done Typing");
+            },
+            1000,
+            () => {
+              setTypingStatus("Deleting...");
+            },
+            "",
+            () => {
+              setTypingStatus("Done Deleting");
+            },
+          ]}
+          repeat={Infinity}
+        />
+        {typingStatus !== "Done Typing" && (
+          <span style={{ color: "red" }}>{typingStatus}</span>
+        )}
+          </small>
+        
+      ) : null}
           welcome <br />
           back!
         </h1>
-        <form
-          onSubmit={formik.handleSubmit}
-          action=""
-          method="post"
-        >
+        <form onSubmit={formik.handleSubmit} action="" method="post">
+        {message && !isSmallScreen ? (
+          <small style={{position: 'absolute', top: '-120px', left: '0'}}>
+          <TypeAnimation
+          sequence={[
+            1500,
+            () => {
+              setTypingStatus("Typing...");
+            },
+            message,
+            () => {
+              setTypingStatus("Done Typing");
+            },
+            1000,
+            () => {
+              setTypingStatus("Deleting...");
+            },
+            "",
+            () => {
+              setTypingStatus("Done Deleting");
+            },
+          ]}
+          repeat={Infinity}
+        />
+        {typingStatus !== "Done Typing" && (
+          <span style={{ color: "red" }}>{typingStatus}</span>
+        )}
+          </small>
+        
+      ) : null}
           <div className="form-control signin-form-control">
             <input
               type="text"
@@ -106,7 +182,7 @@ const SigninForm = () => {
             />
             <div className="password-toggle" onClick={togglePasswordVisibility}>
               {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
-              </div>
+            </div>
             <label>
               <span style={{ transitionDelay: "0ms" }}>P</span>
               <span style={{ transitionDelay: "50ms" }}>a</span>
@@ -123,8 +199,14 @@ const SigninForm = () => {
             <span>
               <input type="checkbox" id="checkBox" /> Remember Me
             </span>
-            
-            <div onClick={()=> navigateToOTP()} className="text-primary" style={{cursor: 'pointer'}}>Forgot Password?</div>
+
+            <div
+              onClick={() => navigateToOTP()}
+              className="text-primary"
+              style={{ cursor: "pointer" }}
+            >
+              Forgot Password?
+            </div>
           </div>
           <button type="submit" className="button mt-5">
             <div className="btn-text">Login</div>
