60089f332e4bb4d3fa99e7943370dbcd447cce31
Adeolu Amole <adeoluamole@gmail.com>
2023-09-03 08:21:01 +0100

    Working on the the questions and answer page

--- src/components/generalComponents/ActivitiesContainer.jsx
+++ src/components/generalComponents/ActivitiesContainer.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
+// import React from 'react'
 
-const ActivitiesContainer = (props) => {
+// const ActivitiesContainer = (props) => {
+//   return (
+//     <>
+//          <div className="d-flex gap-4 col-sm-12 col-md-6 col-lg-4 mb-5">
+//           <i className={props.icon} style={props.style}></i>
+//           <div>
+//             <h4 className='text-capitalize'>{props.title}</h4>
+//             <div className="content mt-3">{props.content}</div>
+//           </div>
+//         </div>
+//     </>
+//   )
+// }
+
+// export default ActivitiesContainer
+
+
+
+import React, { useState } from 'react';
+
+const ActivitiesContainer = ({ title, icon, style, content }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const toggleTooltip = () => {
+    setShowTooltip(!showTooltip);
+  };
+
+  const tooltipStyle = {
+    position: 'absolute',
+    top: '-30px', // Adjust the position of the tooltip
+    left: '50%',
+    transform: 'translateX(-50%)',
+    backgroundColor: '#333',
+    color: '#fff',
+    padding: '5px 10px',
+    borderRadius: '5px',
+    visibility: showTooltip ? 'visible' : 'hidden',
+    opacity: showTooltip ? 1 : 0,
+    transition: 'visibility 0s, opacity 0.3s',
+    zIndex: 1,
+  };
+
   return (
-    <>
-         <div className="d-flex gap-4 col-sm-12 col-md-6 col-lg-4 mb-5">
-          <i className={props.icon} style={props.style}></i>
-          <div>
-            <h4 className='text-capitalize'>{props.title}</h4>
-            <div className="content mt-3">{props.content}</div>
-          </div>
-        </div>
-    </>
-  )
-}
+    <div
+      className="d-flex gap-4 col-sm-12 col-md-6 col-lg-4 mb-5"
+      onMouseEnter={toggleTooltip}
+      onMouseLeave={toggleTooltip}
+      style={{ position: 'relative' }}
+    >
+      <i className={icon} style={style}></i>
+      <div>
+        <h4 className='text-capitalize'>{title}</h4>
+        <div className="content mt-3">{content}</div>
+        <div style={tooltipStyle}>{title}</div>
+      </div>
+    </div>
+  );
+};
 
-export default ActivitiesContainer
+export default ActivitiesContainer;
--- src/components/homepageComponents/OurActivities.jsx
+++ src/components/homepageComponents/OurActivities.jsx
@@ -14,38 +14,38 @@ const OurActivities = () => {
         ></ActivitiesHeader>
         <section className="activities-container mb-5 pb-5 d-flex flex-wrap w-100">
         <ActivitiesContainer
-            title="academic excellence"
+            title="Academic Excellence"
             icon="fas fa-graduation-cap fs-1"
             style={{ color: "#3F51B5" }}
             content="Committed to your child's success, we provide one-on-one tutoring, interactive group study sessions, and personalized learning plans. Our goal is to empower students with essential skills and knowledge, enabling them to surpass academic aspirations and build a brighter future."
           />
           
           <ActivitiesContainer
-            title="many sport"
+            title="Diverse Sports Participation."
             icon="fas fa-person-skiing fs-1"
             style={{ color: "#EC774B" }}
-            content="Our sports programs offer more than just physical activity. They promote holistic development by teaching teamwork, discipline, and sportsmanship. Your child will have the opportunity to engage in various sports, improving their physical fitness, building self-confidence, and creating lasting memories on the field or court.?"
+            content="Our sports programs offer more than just physical activity. They promote holistic development by teaching teamwork, discipline, and sportsmanship. Your child will engage in various sports, improving physical fitness, building self-confidence, and creating lasting memories on the field or court."
           />
           <ActivitiesContainer
-            title="creative"
+            title="Creative Development"
             icon="fas fa-lightbulb text-primary fs-1"
             content="From captivating theater productions to immersive art classes, we encourage self-expression and artistic growth in your child. Our creative arts programs provide a platform for students to explore their talents, expand their horizons, and develop a deep appreciation for the arts, fostering a lifelong love for creativity."
           />
           <ActivitiesContainer
-            title="bus free"
+            title="Free Bus Services"
             icon="fas fa-bus fs-1"
             style={{ color: "#CDDC39" }}
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora maiores ut harum corrupti a sequi atque, voluptas, necessitatibus rem error ad quasi, reprehenderit vel explicabo dolorem at nihil nemo laboriosam?"
+            content="Join us for exhilarating bus-free outdoor expeditions that inspire teamwork, resilience, self-discovery. Students embark on exciting journeys, connecting with nature, bonding with peers, honing leadership skills, creating unforgettable, transformative, life-changing memories."
           />
           <ActivitiesContainer
-            title="community involvement"
+            title="Community Involvement"
             icon="fas fa-hands-holding-child fs-1"
             style={{ color: "#FFC107" }}
-            content="We believe in the importance of giving back. Our community service projects encourage students to actively participate in making a positive impact on their local community. By engaging in meaningful volunteer work and social initiatives, your child will develop a sense of empathy, social responsibility, and a deep connection to the world around them."
+            content="We emphasize giving back and fostering active participation in community service. Through meaningful volunteer work, your child not only develops empathy and social responsibility but also forges a profound connection to the world, shaping their character for life."
           />
           
           <ActivitiesContainer
-            title="excursion and workshops"
+            title="Excursion And Workshops"
             icon="fas fa-earth-europe fs-1"
             style={{ color: "#FF5722" }}
             content="Our dynamic workshops spanning STEM to the arts ignite curiosity, foster creativity, and promote critical thinking in secondary school students. Complemented by exciting excursions to science centers and museums, we provide a holistic and immersive educational experience."
