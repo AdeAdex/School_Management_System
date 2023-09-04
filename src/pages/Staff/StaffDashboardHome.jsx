01a93adf1efc3e37f0989424b1f529cb91067a0d
Adeolu Amole <adeoluamole@gmail.com>
2023-09-04 13:22:07 +0100

    Working on the the questions and answer page

--- src/pages/Staff/StaffDashboardHome.jsx
+++ src/pages/Staff/StaffDashboardHome.jsx
@@ -269,7 +269,7 @@ function MyApp() {
       } = updatedValues;
 
       console.log(valuesWithoutSelectedSubjects);
-      let endpoint = "http://localhost:2000/staff_account/create_staff_account";
+      let endpoint = "https://school-portal-backend-adex2210.vercel.app/staff_account/create_staff_account";
       axios.post(endpoint, valuesWithoutSelectedSubjects).then((response) => {
         setIsLoading(false);
         const Toast = Swal.mixin({
