ce400dd4b79eee2aefc8d46feee9036ae3cffedb
Adeolu Amole <adeoluamole@gmail.com>
2023-09-05 03:43:56 +0100

    Working on the the event page

--- src/components/generalComponents/Parallax1.jsx
+++ src/components/generalComponents/Parallax1.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const Parallax1 = ({
   classes,
   styles,
   inner_content_2
 }) => {
+  const navigate = useNavigate()
+  const goToCoursesPage = () => {
+    navigate('/our_courses')
+  }
   return (
     <>
       <div className={classes} style={styles}>
@@ -13,7 +19,7 @@ const Parallax1 = ({
           style={{ height: "", width: "75%" }}
         >
           <button
-            className="parallax1-btn btn text-white text-uppercase d-flex gap-3 px-4 py-2 justify-content-center"
+            className="parallax1-btn btn text-white text-uppercase d-flex gap-3 px-4 py-2 justify-content-center" onClick={goToCoursesPage}
             style={{ backgroundColor: "#74CEE4", boxShadow: 'inset 0px -4px #54CAE1' }}
           >
             <i className="fas fa-bars my-auto"></i> all courses
