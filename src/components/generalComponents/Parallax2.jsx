78051ae9d7be509630ece141a141ee744218ca36
Adeolu Amole <adeoluamole@gmail.com>
2023-09-10 13:14:04 +0100

    Working on the project perfection

--- src/components/generalComponents/Parallax2.jsx
+++ src/components/generalComponents/Parallax2.jsx
@@ -11,27 +11,27 @@ const Parallax2 = ({ classes, styles, content_styles }) => {
             <br />
             ARE YOU READY ?
           </div>
-          <div className="bg-image2-container center-div2 d-flex col-lg-10 justify-content-between mt-auto">
+          <div className="bg-image2-containe center-di d-flex col-lg-10 justify-content-between mt-auto mx-auto">
             <Countdown
-              id="tm1"
+              // id="tm1"
               styles={{ backgroundColor: "#74CEE4" }}
-              innerText="days"
+              // innerText="days"
             ></Countdown>
-            <Countdown
-              id="tm2"
+            {/* <Countdown
+              // id="tm2"
               styles={{ backgroundColor: "#edbf47" }}
-              innerText="hours"
+              // innerText="hours"
             ></Countdown>
             <Countdown
-              id="tm3"
+              // id="tm3"
               styles={{ backgroundColor: "#ec774b" }}
-              innerText="minutes"
+              // innerText="minutes"
             ></Countdown>
             <Countdown
-              id="tm4"
+              // id="tm4"
               styles={{ backgroundColor: "#6fc191" }}
-              innerText="seconds"
-            ></Countdown>
+              // innerText="seconds"
+            ></Countdown> */}
           </div>
         </div>
       </div>
