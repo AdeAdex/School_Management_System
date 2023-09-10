98e1e25b8086f83ffb23be64bee8318847abb25e
Adeolu Amole <adeoluamole@gmail.com>
2023-09-10 13:27:05 +0100

    Working on the project perfection

--- src/components/generalComponents/Countdown.jsx
+++ src/components/generalComponents/Countdown.jsx
@@ -1,22 +1,66 @@
-// import React from "react";
+
+// import React, { useEffect, useState } from "react";
 
 // const Countdown = ({ id, styles, innerText }) => {
+//   const calculateTimeLeft = () => {
+//     const endDate = new Date("2023-09-10T00:00:00Z"); // Set your countdown end date and time
+//     const currentDate = new Date();
+//     const difference = endDate - currentDate;
+
+//     if (difference <= 0) {
+//       return {
+//         days: "000",
+//         hours: "00",
+//         minutes: "00",
+//         seconds: "00",
+//       };
+//     }
+
+//     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+//     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+//     return {
+//       days: days.toString().padStart(3, "0"),
+//       hours: hours.toString().padStart(2, "0"),
+//       minutes: minutes.toString().padStart(2, "0"),
+//       seconds: seconds.toString().padStart(2, "0"),
+//     };
+//   };
+
+//   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+//   useEffect(() => {
+//     const timer = setInterval(() => {
+//       setTimeLeft(calculateTimeLeft());
+//     }, 1000);
+
+//     return () => clearInterval(timer);
+//   }, []);
+
 //   return (
 //     <>
-//       <div className="">
-//         <div className="margin-y-auto values">000</div>
-//         <div className="text-center text-uppercase rounded py-1" style={styles}>
-//           {innerText}
+//         <div className="values">
+//           {timeLeft.days} days,
 //         </div>
-//       </div>
+//         <div className="values">
+//           {timeLeft.hours} hours, 
+//         </div>
+//         <div className="values">
+//           {timeLeft.minutes} minutes,
+//         </div>
+//         <div className="values">
+//           {timeLeft.seconds} seconds
+//         </div>
+//         {/* <div className="text-center text-uppercase rounded py-1" style={styles}>
+//           {innerText}
+//         </div> */}
 //     </>
 //   );
 // };
 
 // export default Countdown;
-
-
-
 
 
 
@@ -38,7 +82,12 @@ const Countdown = ({ id, styles, innerText }) => {
       };
     }
 
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+    if (days < 10) {
+      days = 10; // Set the initial value of days to 10 if it's less than 10
+    }
+
     const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
@@ -54,30 +103,35 @@ const Countdown = ({ id, styles, innerText }) => {
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {
-    const timer = setInterval(() => {
+    // Function to update the countdown
+    const updateCountdown = () => {
       setTimeLeft(calculateTimeLeft());
-    }, 1000);
+    };
 
+    // Call the updateCountdown function immediately
+    updateCountdown();
+
+    // Set up an interval to update the countdown every second
+    const timer = setInterval(updateCountdown, 1000);
+
+    // Clean up the interval when the component unmounts
     return () => clearInterval(timer);
   }, []);
 
   return (
     <>
-        <div className="values">
-          {timeLeft.days} days,
-        </div>
-        <div className="values">
-          {timeLeft.hours} hours, 
-        </div>
-        <div className="values">
-          {timeLeft.minutes} minutes,
-        </div>
-        <div className="values">
-          {timeLeft.seconds} seconds
-        </div>
-        {/* <div className="text-center text-uppercase rounded py-1" style={styles}>
-          {innerText}
-        </div> */}
+      <div className="values">
+        {timeLeft.days} days,
+      </div>
+      <div className="values">
+        {timeLeft.hours} hours,
+      </div>
+      <div className="values">
+        {timeLeft.minutes} minutes,
+      </div>
+      <div className="values">
+        {timeLeft.seconds} seconds
+      </div>
     </>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,41 +13,41 @@ import ScrollProgress from "../components/generalComponents/ScrollProgress";
 // import MyCountUp from "../components/MyCountUp";
 
 const HomePage = () => {
-  const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
+  // const [count, setCount] = useState(0);
+  // const [calculation, setCalculation] = useState(0);
 
-  useEffect(() => {
-    setCalculation(() => count * 2);
-  }, [count]);
+  // useEffect(() => {
+  //   setCalculation(() => count * 2);
+  // }, [count]);
 
-  var countDownDate = new Date("Jun 1, 2023 12:00:00").getTime();
-  const myDate = new Date();
-  const days = myDate.getHours();
-  console.log(myDate);
-  console.log(days);
-  var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // var countDownDate = new Date("Jun 1, 2023 12:00:00").getTime();
+  // const myDate = new Date();
+  // const days = myDate.getHours();
+  // console.log(myDate);
+  // console.log(days);
+  // var x = setInterval(function () {
+  //   var now = new Date().getTime();
+  //   var distance = countDownDate - now;
+  //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //   var hours = Math.floor(
+  //     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  //   );
+  //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    tm1.innerHTML = days;
-    tm2.innerHTML = hours;
-    tm3.innerHTML = minutes;
-    tm4.innerHTML = seconds;
+  //   tm1.innerHTML = days;
+  //   tm2.innerHTML = hours;
+  //   tm3.innerHTML = minutes;
+  //   tm4.innerHTML = seconds;
 
-    if (distance < 0) {
-      clearInterval(x);
-      tm1.innerHTML = "000";
-      tm2.innerHTML = "00";
-      tm3.innerHTML = "00";
-      tm4.innerHTML = "00";
-    }
-  }, 1000);
+  //   if (distance < 0) {
+  //     clearInterval(x);
+  //     tm1.innerHTML = "000";
+  //     tm2.innerHTML = "00";
+  //     tm3.innerHTML = "00";
+  //     tm4.innerHTML = "00";
+  //   }
+  // }, 1000);
 
   return (
     <>
