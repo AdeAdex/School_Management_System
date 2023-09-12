cb82d06769c92b7866da17311326519aebea4d28
Adeolu Amole <adeoluamole@gmail.com>
2023-09-12 14:23:10 +0100

    Working on the project perfection

--- src/components/footerComponents/Contact_Us_Form.jsx
+++ src/components/footerComponents/Contact_Us_Form.jsx
@@ -1,10 +1,12 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 const Contact_Us_Form = () => {
   const contactUsRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const validationSchema = Yup.object().shape({
     name: Yup.string().required("Name is required"),
@@ -22,18 +24,36 @@ const Contact_Us_Form = () => {
     },
     validationSchema: validationSchema,
     onSubmit: (values, { resetForm }) => {
+      setIsLoading(true);
       let endpoint = 'https://school-portal-backend-adex2210.vercel.app/staff_account/people_reaching_us'
       axios.post(endpoint, values)
       .then((response) => {
+      setIsLoading(false);
         const Toast = Swal.mixin({
           toast: true,
-          position: "top",
+          position: "bottom",
           showConfirmButton: false,
-          timer: 4000,
-          timerProgressBar: true,
+          html: `
+            <div class="toast-content">
+              <button id="close-toast-button" class="close-button">&times;</button>
+            </div>
+          `,
           didOpen: (toast) => {
             toast.addEventListener("mouseenter", Swal.stopTimer);
             toast.addEventListener("mouseleave", Swal.resumeTimer);
+
+            const closeButton = document.getElementById("close-toast-button");
+            closeButton.addEventListener("click", () => {
+              Swal.close(); // Close the toast
+            });
+            
+            document.body.addEventListener("click", () => {
+              Swal.close(); // Close the toast
+            });
+          },
+          customClass: {
+            container: "custom-toast-width",
+            popup: "custom-toast-position",
           },
         });
 
@@ -93,7 +113,7 @@ const Contact_Us_Form = () => {
         ) : null}
 
         <button className="footer-contact-btn" type="submit">
-          Send
+        {isLoading ? <div className="spinner"></div> : <div>Send </div>}
         </button>
       </form>
     </div>
--- src/pages/ContactUSPage.jsx
+++ src/pages/ContactUSPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
@@ -7,6 +7,8 @@ import PagesNavbar from "../components/navbarComponents/PagesNavbar";
 import Footer from "../components/footerComponents/Footer";
 
 const ContactUSPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const validationSchema = Yup.object().shape({
     name: Yup.string().required("Name is required"),
     email: Yup.string()
@@ -23,20 +25,38 @@ const ContactUSPage = () => {
     },
     validationSchema: validationSchema,
     onSubmit: (values, { resetForm }) => {
+      setIsLoading(true);
       let endpoint =
         "https://school-portal-backend-adex2210.vercel.app/staff_account/people_reaching_us";
       axios
         .post(endpoint, values)
         .then((response) => {
+          setIsLoading(false);
           const Toast = Swal.mixin({
             toast: true,
-            position: "top",
+            position: "bottom",
             showConfirmButton: false,
-            timer: 4000,
-            timerProgressBar: true,
+            html: `
+              <div class="toast-content">
+                <button id="close-toast-button" class="close-button">&times;</button>
+              </div>
+            `,
             didOpen: (toast) => {
               toast.addEventListener("mouseenter", Swal.stopTimer);
               toast.addEventListener("mouseleave", Swal.resumeTimer);
+
+              const closeButton = document.getElementById("close-toast-button");
+              closeButton.addEventListener("click", () => {
+                Swal.close(); // Close the toast
+              });
+
+              document.body.addEventListener("click", () => {
+                Swal.close(); // Close the toast
+              });
+            },
+            customClass: {
+              container: "custom-toast-width",
+              popup: "custom-toast-position",
             },
           });
 
@@ -81,7 +101,7 @@ const ContactUSPage = () => {
                   type="text"
                   required=""
                   placeholder="Your Name"
-                  className="text-capitalize"
+                  className="text-capitalize contact-form-container-input"
                   name="name"
                   value={formik.values.name}
                   onChange={formik.handleChange}
@@ -98,6 +118,7 @@ const ContactUSPage = () => {
                   name="email"
                   required=""
                   placeholder="Your Email"
+                  className="contact-form-container-input"
                   autoComplete="off"
                   value={formik.values.email}
                   onChange={formik.handleChange}
@@ -113,9 +134,10 @@ const ContactUSPage = () => {
                   name="message"
                   required=""
                   placeholder="Your Message"
-                  className="text-capitalize textarea"
+                  className="text-capitalize contact-form-container-input"
                   value={formik.values.message}
                   onChange={formik.handleChange}
+                  rows={5}
                 />
                 {formik.touched.message && formik.errors.message ? (
                   <div className="error text-danger">
@@ -124,7 +146,9 @@ const ContactUSPage = () => {
                 ) : null}
               </div>
 
-              <button type="submit">Send</button>
+              <button type="submit" className="contact-form-submit-btn">
+                {isLoading ? <div className="spinner"></div> : <div>Send </div>}
+              </button>
             </form>
           </div>
           <div className="map">
