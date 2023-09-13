ad15cb4e43d33fdb4894a7372ec2a7f387342242
Adeolu Amole <adeoluamole@gmail.com>
2023-09-13 11:40:49 +0100

    Working on the project perfection

--- src/components/studentDashboardComponents/StudentDashboardPieChart.jsx
+++ src/components/studentDashboardComponents/StudentDashboardPieChart.jsx
@@ -12,30 +12,101 @@ const StudentDashboardPieChart = () => {
     let endpoint = "http://localhost:2000/student_account/student_performance";
     axios.post(endpoint, { studentEmail }).then((response) => {
       if (response.data.status) {
-        // console.log();
         setPerformance(response.data.res);
       }
     });
   }, [globalState]);
 
+  // const categoryPercentages = {};
+  // performance.forEach((item) => {
+  //   if (!categoryPercentages[item.category]) {
+  //     categoryPercentages[item.category] = { correct: 0, total: 0 };
+  //   }
+  //   if (item.correctness) {
+  //     categoryPercentages[item.category].correct++;
+  //   }
+  //   categoryPercentages[item.category].total++;
+  // });
+
+  // const pieChartData = Object.keys(categoryPercentages).map((category) => ({
+  //   label: category, // This sets the category name as the label
+  //   value: (categoryPercentages[category].correct / categoryPercentages[category].total) * 100,
+  //   color: getRandomColor(), // Define a function to generate unique colors
+  // }));
+
+  // // Function to generate random colors (you can replace this)
+  // function getRandomColor() {
+  //   const letters = "0123456789ABCDEF";
+  //   let color = "#";
+  //   for (let i = 0; i < 6; i++) {
+  //     color += letters[Math.floor(Math.random() * 16)];
+  //   }
+  //   return color;
+  // }
+
+  const categoryPercentages = performance.reduce((acc, item) => {
+    if (!acc[item.category]) {
+      acc[item.category] = { correct: 0, total: 0 };
+    }
+    if (item.correctness) {
+      acc[item.category].correct++;
+    }
+    acc[item.category].total++;
+    return acc;
+  }, {});
+
+
+
+   // Helper function to generate random colors
+   const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  // Convert percentages to PieChart data
+  const pieChartData = Object.keys(categoryPercentages).map((category) => ({
+    title: category,
+    value:
+      (categoryPercentages[category].correct /
+        categoryPercentages[category].total) *
+      100,
+    color: getRandomColor(),
+  }));
+
+ 
+
   return (
     <>
-      <PieChart
+      {/* <PieChart
         className="mx-auto"
         style={{ width: "80%" }}
         data={[
           {
-            label: "One",
+            label: "English",
             value: 10,
             color: "#E38627",
             animate: true,
             animationDuration: 500,
             animationEasing: "linear",
           },
-          { label: "Two", value: 15, color: "#C13C37" },
-          { label: "Three", value: 20, color: "#6A2135" },
-          { label: "Four", value: 20, color: "#6fc191" },
+          { label: "Math", value: 15, color: "#C13C37" },
+          { label: "Current Affair", value: 20, color: "#6A2135" },
+          { label: "Sciences", value: 20, color: "#6fc191" },
         ]}
+      /> */}
+
+      <PieChart
+        className="mx-auto"
+        style={{ width: "80%" }}
+        data={pieChartData}
+        label={({ dataEntry }) =>
+          `${dataEntry.title}: ${Math.round(dataEntry.value)}%`
+        } // Display labels with title and percentage
+        labelStyle={{ fontSize: "5px" }} // Adjust label font size as needed
       />
     </>
   );
