2b1b9cece72c6e73e373dad1be799ab1df45fbd4
Adeolu Amole <adeoluamole@gmail.com>
2023-09-13 10:53:39 +0100

    Working on the project perfection

--- src/components/studentDashboardComponents/StudentDashboardPieChart.jsx
+++ src/components/studentDashboardComponents/StudentDashboardPieChart.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { PieChart } from 'react-minimal-pie-chart'
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+
 
 const StudentDashboardPieChart = () => {
+  let globalState = useSelector((state) => state.portalReducer.studentInfo);
+
+  useEffect(() => {
+    let studentEmail = globalState.email
+    let endpoint = "http://localhost:2000/student_account/student_performance"
+    axios.post(endpoint, studentEmail)
+    .then((response) => {
+      console.log(response);
+    })
+  }, [])
+  
   return (
     <>
     <PieChart className='mx-auto' style={{width: '80%'}}
