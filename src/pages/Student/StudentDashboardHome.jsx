d8589c1fe2cf5310c41c7334ac3d6fa764e01849
Adeolu Amole <adeoluamole@gmail.com>
2023-09-13 10:42:55 +0100

    Working on the project perfection

--- src/pages/Student/StudentDashboardHome.jsx
+++ src/pages/Student/StudentDashboardHome.jsx
@@ -11,7 +11,7 @@ const StudentDashboardHome = () => {
     <>
       <div className="d-flex flex-lg-row flex-column justify-content-between dashboard-home-container w-100 h-100">
         <div className=" text-black">
-          <small>Welcome to your dashboard: <span>You created an account with us on: {globalState.createdDate}</span></small>
+          <small className="d-flex flex-column px-4">Welcome to your dashboard: <span className="my-3">You created an account with us on: {globalState.createdDate}</span></small>
         </div>
         <div className="dashboard-home-container-right d-flex shadow p-3 flex-column justify-content-center gap-2" style={{width: '350px', marginLeft: 'auto'}}>
           <StudentDashboardCalendar />
