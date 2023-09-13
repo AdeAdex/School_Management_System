adba8aee9289d14c60c61ac4aea20e3567db7316
Adeolu Amole <adeoluamole@gmail.com>
2023-09-13 11:25:46 +0100

    Working on the project perfection

--- src/components/studentDashboardComponents/StudentDashboardPieChart.jsx
+++ src/components/studentDashboardComponents/StudentDashboardPieChart.jsx
@@ -1,35 +1,44 @@
-import React, { useEffect } from 'react'
-import { PieChart } from 'react-minimal-pie-chart'
+import React, { useEffect, useState } from "react";
+import { PieChart } from "react-minimal-pie-chart";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
-
-
 const StudentDashboardPieChart = () => {
   let globalState = useSelector((state) => state.portalReducer.studentInfo);
+  const [performance, setPerformance] = useState([]);
 
   useEffect(() => {
-    let studentEmail = globalState.email
-    let endpoint = "http://localhost:2000/student_account/student_performance"
-    axios.post(endpoint, { studentEmail })
-    .then((response) => {
+    let studentEmail = globalState.email;
+    let endpoint = "http://localhost:2000/student_account/student_performance";
+    axios.post(endpoint, { studentEmail }).then((response) => {
       if (response.data.status) {
-        console.log(response.data.res);
+        // console.log();
+        setPerformance(response.data.res);
       }
-    })
-  }, [globalState])
-  
+    });
+  }, [globalState]);
+
   return (
     <>
-    <PieChart className='mx-auto' style={{width: '80%'}}
+      <PieChart
+        className="mx-auto"
+        style={{ width: "80%" }}
         data={[
-          { title: "One", value: 10, color: "#E38627" , animate: true, animationDuration:500, animationEasing: "linear" },
-          { title: "Two", value: 15, color: "#C13C37" },        
-          { title: "Three", value: 20, color: "#6A2135" },
+          {
+            label: "One",
+            value: 10,
+            color: "#E38627",
+            animate: true,
+            animationDuration: 500,
+            animationEasing: "linear",
+          },
+          { label: "Two", value: 15, color: "#C13C37" },
+          { label: "Three", value: 20, color: "#6A2135" },
+          { label: "Four", value: 20, color: "#6fc191" },
         ]}
       />
     </>
-  )
-}
+  );
+};
 
-export default StudentDashboardPieChart
+export default StudentDashboardPieChart;
