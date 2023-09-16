8b215deb96ea7825257a761c01291337369996cd
Adeolu Amole <adeoluamole@gmail.com>
2023-09-16 02:51:30 +0100

    Working on the project perfection

--- src/components/studentProfileComponents.jsx/Personal.jsx
+++ src/components/studentProfileComponents.jsx/Personal.jsx
@@ -10,7 +10,7 @@ const Personal = () => {
       <div className="w-100 d-flex flex-wrap gap-4">
         <EachInfo label="Surname" value={globalState.lastName}/>
         <EachInfo label="First Name" value={globalState.firstName}/>
-        <EachInfo label="middle name" value={globalState.lastName}/>
+        <EachInfo label="middle name" value={globalState.middleName}/>
         <EachInfo label="age" value={globalState.age}/>
         <EachInfo label="nationality" value={globalState.country}/>
         <EachInfo label="state of origin" value={globalState.state}/>
--- src/pages/Student/StudentEditDetails.jsx
+++ src/pages/Student/StudentEditDetails.jsx
@@ -179,6 +179,7 @@ function MyApp() {
         </Stepper.Step>
         {/* <Stepper.Completed>Completed</Stepper.Completed> */}
       </Stepper>
+      
     </>
   );
 }
