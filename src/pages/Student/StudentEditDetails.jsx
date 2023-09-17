567a23ffcdb8f87a9809f65e788515d7186df1d1
Adeolu Amole <adeoluamole@gmail.com>
2023-09-17 22:13:09 +0100

    Working on the project perfection

--- src/pages/Student/StudentEditDetails.jsx
+++ src/pages/Student/StudentEditDetails.jsx
@@ -148,17 +148,13 @@ function MyApp() {
   const [gender, setGender] = useState(globalState.gender || "");
   const [title, setTitle] = useState(globalState.myTitle || "");
   const [age, setAge] = useState(globalState.age || "");
+  const [phoneNumber, setPhoneNumber] = useState(globalState.phoneNumber || "");
+  const [email, setEmail] = useState(globalState.email || "");
+  const [address, setAddress] = useState(globalState.address || "");
+  const [refereeName, setRefereeName] = useState(globalState.refereeName || "");
+  const [refereePhoneNumber, setRefereePhoneNumber] = useState(globalState.refereePhoneNumber || "");
+  const [refereeAddress, setRefereeAddress] = useState(globalState.refereeAddress || "");
 
-
-  const hasRequiredData =
-    globalState.lastName &&
-    globalState.firstName &&
-    globalState.middleName &&
-    globalState.country &&
-    globalState.state &&
-    globalState.gender &&
-    globalState.myTitle &&
-    globalState.age;
 
   const [active, setActive] = useState(0);
   const nextStep = (variant) =>
@@ -210,7 +206,13 @@ function MyApp() {
         country,
         state,
         gender,
-        title
+        title,
+        phoneNumber,
+        email,
+        address,
+        refereeName,
+        refereePhoneNumber,
+        refereeAddress
       );
     }
   };
@@ -230,122 +232,122 @@ function MyApp() {
           icon={<FaUserCheck size="1.1rem" />}
           description="Personal"
         >
-        {hasRequiredData && (
-          <div className="w-100 d-flex flex-wrap gap-4">
-          {/* <input type="text" name="" onChange={(e) => setLastName(e.target.value)} value={lastName} id="" /> */}
-            <EachInfo
-              label="Surname"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-              enabled={enabled}
-            />
-            <EachInfo
-              label="First Name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-              enabled={enabled}
-            />
-            <EachInfo
-              label="Middle name"
-              value={middleName}
-              onChange={(e) => setMiddleName(e.target.value)}
-              enabled={enabled}
-            />
-            <EachInfo
-              label="Age"
-              value={age}
-              onChange={(e) => setAge(e.target.value)}
-              enabled={enabled}
-            />
-            <div className="each-info" style={{ width: "48%" }}>
-              <Box
-                component="form"
-                sx={{
-                  "& .MuiTextField-root": { m: 0, width: "100%" },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField
-                  disabled={!enabled}
-                  id="standard-select-currency"
-                  select
-                  label="Nationality"
-                  name="country"
-                  value={country || ""}
-                  variant="standard"
-                  onChange={(e) => {
-                    handleCountryChange(e);
-                    setCountry(e.target.value);
+          {hasRequiredData && (
+            <div className="w-100 d-flex flex-wrap gap-4">
+              {/* <input type="text" name="" onChange={(e) => setLastName(e.target.value)} value={lastName} id="" /> */}
+              <EachInfo
+                label="Surname"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                enabled={enabled}
+              />
+              <EachInfo
+                label="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                enabled={enabled}
+              />
+              <EachInfo
+                label="Middle name"
+                value={middleName}
+                onChange={(e) => setMiddleName(e.target.value)}
+                enabled={enabled}
+              />
+              <EachInfo
+                label="Age"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+                enabled={enabled}
+              />
+              <div className="each-info" style={{ width: "48%" }}>
+                <Box
+                  component="form"
+                  sx={{
+                    "& .MuiTextField-root": { m: 0, width: "100%" },
                   }}
+                  noValidate
+                  autoComplete="off"
                 >
-                  {allCountry
-                    .slice() // Create a copy to avoid modifying the original array
-                    .sort((a, b) => a.country.localeCompare(b.country)) // Sort alphabetically
-                    .map((eachCountry, index) => (
-                      <MenuItem
-                        key={eachCountry.id}
-                        value={eachCountry.country}
-                      >
-                        {eachCountry.country}
-                      </MenuItem>
-                    ))}
-                </TextField>
-              </Box>
-            </div>
+                  <TextField
+                    disabled={!enabled}
+                    id="standard-select-currency"
+                    select
+                    label="Nationality"
+                    name="country"
+                    value={country || ""}
+                    variant="standard"
+                    onChange={(e) => {
+                      handleCountryChange(e);
+                      setCountry(e.target.value);
+                    }}
+                  >
+                    {allCountry
+                      .slice() // Create a copy to avoid modifying the original array
+                      .sort((a, b) => a.country.localeCompare(b.country)) // Sort alphabetically
+                      .map((eachCountry, index) => (
+                        <MenuItem
+                          key={eachCountry.id}
+                          value={eachCountry.country}
+                        >
+                          {eachCountry.country}
+                        </MenuItem>
+                      ))}
+                  </TextField>
+                </Box>
+              </div>
 
-            <div className="each-info" style={{ width: "48%" }}>
-              <Box
-                component="form"
-                sx={{
-                  "& .MuiTextField-root": { m: 0, width: "100%" },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField
-                  disabled={!enabled}
-                  id="standard-select-currency"
-                  select
-                  label="State of origin"
-                  name="state"
-                  value={state || ""}
-                  variant="standard"
-                  onChange={(e) => {
-                    setState(e.target.value);
+              <div className="each-info" style={{ width: "48%" }}>
+                <Box
+                  component="form"
+                  sx={{
+                    "& .MuiTextField-root": { m: 0, width: "100%" },
                   }}
+                  noValidate
+                  autoComplete="off"
                 >
-                  {statesForCountry.length === 0 && globalState.state ? (
-                    <MenuItem value={globalState.state}>
-                      {globalState.state}
-                    </MenuItem>
-                  ) : null}
-                  {statesForCountry
-                    .slice() // Create a copy to avoid modifying the original array
-                    .sort() // Sort the array alphabetically
-                    .map((selectedState) => (
-                      <MenuItem key={selectedState} value={selectedState}>
-                        {selectedState}
+                  <TextField
+                    disabled={!enabled}
+                    id="standard-select-currency"
+                    select
+                    label="State of origin"
+                    name="state"
+                    value={state || ""}
+                    variant="standard"
+                    onChange={(e) => {
+                      setState(e.target.value);
+                    }}
+                  >
+                    {statesForCountry.length === 0 && globalState.state ? (
+                      <MenuItem value={globalState.state}>
+                        {globalState.state}
                       </MenuItem>
-                    ))}
-                </TextField>
-              </Box>
-            </div>
-            {/* <EachInfoForSelect label="Nationality" value={globalState.country}/>
+                    ) : null}
+                    {statesForCountry
+                      .slice() // Create a copy to avoid modifying the original array
+                      .sort() // Sort the array alphabetically
+                      .map((selectedState) => (
+                        <MenuItem key={selectedState} value={selectedState}>
+                          {selectedState}
+                        </MenuItem>
+                      ))}
+                  </TextField>
+                </Box>
+              </div>
+              {/* <EachInfoForSelect label="Nationality" value={globalState.country}/>
         <EachInfoForSelectState label="State of origin" value={globalState.state}/> */}
-            <EachInfo
-              label="Gender"
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-              enabled={enabled}
-            />
-            <EachInfo
-              label="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              enabled={enabled}
-            />
-          </div>
+              <EachInfo
+                label="Gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                enabled={enabled}
+              />
+              <EachInfo
+                label="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                enabled={enabled}
+              />
+            </div>
           )}
           {/* <Personal enabled={enabled} edit={edit} sendPayloadToBackend={sendPayloadToBackend}/> */}
           <Group position="center" mt="">
@@ -360,10 +362,30 @@ function MyApp() {
           description="Contact"
         >
           <div className="w-100 d-flex flex-wrap gap-4">
-            <EachInfo label="phone number 1" value={globalState.phoneNumber} />
-            <EachInfo label="phone number 2" value={globalState.phoneNumber} />
-            <EachInfo label="email" value={globalState.email} />
-            <EachInfo label="address" value={globalState.address} />
+            <EachInfo
+              label="phone number 1"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              enabled={enabled}
+            />
+            <EachInfo
+              label="phone number 2"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              enabled={enabled}
+            />
+            <EachInfo
+              label="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              enabled={enabled}
+            />
+            <EachInfo
+              label="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              enabled={enabled}
+            />
           </div>
           {/* <Contact /> */}
           <Group position="center" mt="xl">
@@ -374,7 +396,27 @@ function MyApp() {
           </Group>
         </Stepper.Step>
         <Stepper.Step label="" description="Referee">
-          <Referee />
+          <div className="w-100 d-flex flex-wrap gap-4">
+            <EachInfo
+              label="full Name"
+              value={refereeName}
+              onChange={(e) => setRefereeName(e.target.value)}
+              enabled={enabled}
+            />
+            <EachInfo
+              label="Phone Number"
+              value={refereePhoneNumber}
+              onChange={(e) => setRefereePhoneNumber(e.target.value)}
+              enabled={enabled}
+            />
+            <EachInfo
+              label="email"
+              value={refereeAddress}
+              onChange={(e) => setRefereeAddress(e.target.value)}
+              enabled={enabled}
+            />
+          </div>
+          {/* <Referee /> */}
           <Group position="center" mt="xl">
             <Button variant="default" onClick={prevStep}>
               Back
