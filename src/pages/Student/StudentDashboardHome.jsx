ea57df3e350ce7ef21f99cadb6d3bb35772f70bc
Adeolu Amole <adeoluamole@gmail.com>
2023-09-20 14:43:05 +0100

    Working on the project perfection

--- src/pages/Student/StudentDashboardHome.jsx
+++ src/pages/Student/StudentDashboardHome.jsx
@@ -4,6 +4,7 @@ import StudentDashboardPieChart from "../../components/studentDashboardComponent
 import { useSelector } from "react-redux";
 import { FaRegUser, FaUser, FaGraduationCap, FaCalendarAlt, FaBuilding } from "react-icons/fa";
 import StudentDetails from "../../components/studentDashboardComponents/StudentDetails";
+import "../Student/StudentDashboardHome.css"
 
 
 const StudentDashboardHome = () => {
@@ -13,25 +14,25 @@ const StudentDashboardHome = () => {
     <>
       <div className="d-flex flex-lg-row flex-column justify-content-between dashboard-home-container w-100 h-100 gap-2">
         <div className="text-black w-100 pe-3 dashboard-home-container-left">
-          <small className="d-flex flex-column px-4 mb-5 text-white py-4" style={{backgroundColor: '#030552', borderRadius: '5px'}}><h6 className="text-uppercase">Welcome to your dashboard: {globalState.firstName} {" "}  {globalState.lastName}</h6><span className="my-3">You created an account with us on: {globalState.createdDate}</span></small>
+          <small className="d-flex flex-column px-4 mb-5 text-white py-4" style={{backgroundColor: '#030552', borderRadius: '5px'}}><h6 className="welcome-msg-on-dashbord text-uppercase d-flex gap-2">Welcome to your dashboard: <span> {globalState.firstName} {" "}  {globalState.lastName}</span></h6><span className="my-3">You created an account with us on: {globalState.createdDate}</span></small>
           <div className="d-flex gap-3 w-100" style={{flexWrap: 'wrap'}}>
           <StudentDetails
-            icons={<FaUser />}
+            icons={<FaUser color="skyblue"/>}
             param={`${globalState.studentID}`}
             about="student Id"
           />
           <StudentDetails
-            icons={<FaGraduationCap />}
+            icons={<FaGraduationCap color="blue" />}
             param={`${globalState.level}`}
             about="Current Class"
           />
           <StudentDetails
-            icons={<FaCalendarAlt />}
+            icons={<FaCalendarAlt color="salmon" />}
             param={`${globalState.term}`}
             about="Current Term"
           />
           <StudentDetails
-            icons={<FaBuilding />}
+            icons={<FaBuilding color="teal" />}
             param={`${globalState.options}`}
             about="Faculty"
           />
