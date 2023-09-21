c86704d8e7f8b0e5fead534954d52ec5ee67b82f
Adeolu Amole <adeoluamole@gmail.com>
2023-09-21 17:43:00 +0100

    Working on the project perfection

--- src/pages/Student/StudentRegistrationHistory.jsx
+++ src/pages/Student/StudentRegistrationHistory.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import { useSelector } from 'react-redux';
 
 const StudentRegistrationHistory = () => {
+
+  const globalState = useSelector((state) => state.portalReducer.studentInfo);
+
+  const receivedEmail = globalState.email;
+  const formClass = globalState.level;
+  const formTerm = globalState.term;
+  const formOption = globalState.options;
+
+  const [yhea, setYhea] = useState([]);
+
+  useEffect(() => {    
+    let endpoint2 =
+      "https://school-portal-backend-adex2210.vercel.app/student_account/student_term_subject";
+    axios
+      .get(endpoint2, {
+        params: {
+          receivedEmail,
+          formClass,
+          formTerm,
+          formOption,
+        },
+      })
+      .then((response) => {
+        setYhea(response.data);
+      });
+  }, [globalState]);
+
+
   return (
-    <div className='d-flex flex-column p-4'>
-        <small>StudentRegistrationHistory </small>{""}
-        <div>Coming soon...</div>
-    </div>
+   <>
+   <div className="w-100 px-2 course-reg-container" style={{height: '100%', backgroundColor: ''}}>
+   <div className="" style={{overflowY: 'auto', height: '70%'}}>
+          <table className="table table-borderd mt-4">
+            <thead>
+              <tr>
+                <td>Subject Title</td>
+                <td>Term</td>
+                <td>Options</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {yhea && yhea.length > 0 ? (
+                yhea.map((subject, index) => (
+                  <tr key={index}>
+                    <td>{subject.mySubject}</td>
+                    <td>{subject.newTerm}</td>
+                    <td>{subject.myOption}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-white shadow delete-btn"
+                        onClick={() => deleteSelectedSubject(subject._id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4" className="text-center">
+                    No Results Found. Add subjects.
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+   </div>
+    
+   </>
   )
 }
 
