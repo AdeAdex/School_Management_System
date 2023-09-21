806d57de52982f25b514ddfe0f97e33da41813e6
Adeolu Amole <adeoluamole@gmail.com>
2023-09-21 17:33:22 +0100

    Working on the project perfection

--- src/pages/Student/StudentCourseRegistration.jsx
+++ src/pages/Student/StudentCourseRegistration.jsx
@@ -79,7 +79,22 @@ const StudentCourseRegistration = () => {
           receivedEmail,
         })
         .then((response) => {
-          // console.log(response.data.selectedArray);
+          const Toast = Swal.mixin({
+            toast: true,
+            position: "top",
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
+
+          Toast.fire({
+            icon: "success",
+            title: response.data.message,
+          });
           dispatch(mySubSub(response.data.selectedArray));
         });
     }
