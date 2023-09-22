cc9a6065f9da45d78452202bde669b539e2c832b
Adeolu Amole <adeoluamole@gmail.com>
2023-09-22 03:42:24 +0100

    Working on the project perfection

--- src/pages/Student/Payment.jsx
+++ src/pages/Student/Payment.jsx
@@ -93,7 +93,7 @@ const Payment = ({
             reference: response.reference
           };
           let endpoint =
-            "http://localhost:2000/student_account/paidAdmissionFee";
+            "https://school-portal-backend-adex2210.vercel.app/student_account/paidAdmissionFee";
           axios
             .post(endpoint, payload)
             .then((response) => {
