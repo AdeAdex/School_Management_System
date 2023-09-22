acf7e63ea6fb66fa502aad36f17492a3d76afff2
Adeolu Amole <adeoluamole@gmail.com>
2023-09-22 17:46:30 +0100

    Working on the project perfection

--- src/pages/Student/PaymentPrintPage.jsx
+++ src/pages/Student/PaymentPrintPage.jsx
@@ -5,67 +5,71 @@ import { useSelector } from "react-redux";
 
 const PaymentPrintPage = () => {
   const location = useLocation();
-  const {payment, studentInfo} = location.state;
+  const { payment, studentInfo } = location.state;
 
   useEffect(() => {
-        window.print();
+    window.print();
   }, [location, payment, studentInfo]);
   return (
     <>
       <div className="payment-container">
-      <div className="payment-details">
-        {studentInfo && payment ? (
-          <table className="payment-table">
-            <tbody>
-              <tr>
-                <td className="student-photo">
-                  <img src={studentInfo.profileURL} alt={studentInfo.firstName} />
-                </td>
-                <td className="student-details">
-                  <h2>Payment Details</h2>
-                  <div className="student-info">
+        <div className="payment-details">
+          {studentInfo && payment ? (
+            <table className="payment-table">
+              <tbody>
+                <tr>
+                  <td className="student-photo">
+                    <img
+                      src={studentInfo.profileURL}
+                      alt={studentInfo.firstName}
+                    />
+                  </td>
+                  <td className="student-details">
+                    <h2>Payment Details</h2>
+                    <div className="student-info">
+                      <p>
+                        <strong>Name:</strong> {studentInfo.firstName}{" "}
+                        {studentInfo.lastName}
+                      </p>
+                      <p>
+                        <strong>Gender:</strong> {studentInfo.gender}
+                      </p>
+                      <p>
+                        <strong>Student ID:</strong> {studentInfo.studentID}
+                      </p>
+                      <p>
+                        <strong>Level:</strong> {studentInfo.level}
+                      </p>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="2">
+                    <hr />
+                    <h3>Fee Breakdown</h3>
+                    <div className="fee-breakdown">
+                      <p>
+                        <strong>Amount To Pay:</strong> ₦{payment.amountToPaid}
+                      </p>
+                      <p>
+                        <strong>Amount Paid:</strong> ₦{payment.amountPaid}
+                      </p>
+                      <p>
+                        <strong>Balance:</strong> ₦{payment.balance}
+                      </p>
+                    </div>
                     <p>
-                      <strong>Name:</strong> {studentInfo.firstName} {studentInfo.lastName}
+                      <strong>Date:</strong> {payment.date}
                     </p>
-                    <p>
-                      <strong>Gender:</strong> {studentInfo.gender}
-                    </p>
-                    <p>
-                      <strong>Student ID:</strong> {studentInfo.studentID}
-                    </p>
-                    <p>
-                      <strong>Level:</strong> {studentInfo.level}
-                    </p>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td colSpan="2">
-                  <hr />
-                  <h3>Fee Breakdown</h3>
-                  <div className="fee-breakdown">
-                    <p>
-                      <strong>Amount To Pay:</strong> ₦{payment.amountToPaid}
-                    </p>
-                    <p>
-                      <strong>Amount Paid:</strong> ₦{payment.amountPaid}
-                    </p>
-                    <p>
-                      <strong>Balance:</strong> ₦{payment.balance}
-                    </p>
-                  </div>
-                  <p>
-                    <strong>Date:</strong> {payment.date}
-                  </p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        ) : (
-          <p>No student or payment details found.</p>
-        )}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          ) : (
+            <p>No student or payment details found.</p>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
