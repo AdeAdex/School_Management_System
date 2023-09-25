502dcd34a367e1c85222eb4ee92b6572fdec84bf
Adeolu Amole <adeoluamole@gmail.com>
2023-09-25 12:09:43 +0100

    Working on the project perfection

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -15,9 +15,6 @@ const AboutUsPage = () => {
       navigator.geolocation.getCurrentPosition(function (position) {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-        console.log(position);
-        console.log("Latitude: " + latitude);
-        console.log("Longitude: " + longitude);
       });
     } else {
       console.log("Geolocation is not available in this browser.");
@@ -34,15 +31,50 @@ const AboutUsPage = () => {
     getMyLocation();
     fetchSchoolLocation();
   }, []);
- 
 
   const handleLocateUsClick = (e) => {
     e.preventDefault();
     if (schoolLocation) {
       const googleMapsLink = `https://maps.google.com/?q=${schoolLocation.lat},${schoolLocation.lng}`;
-      window.open(googleMapsLink, '_blank'); // Opens the link in a new tab
+      window.open(googleMapsLink, "_blank"); // Opens the link in a new tab
     }
   };
+
+  const mapStyle = {
+    backgroundImage: `url('https://res.cloudinary.com/dn4gfzlhq/image/upload/v1695638407/map_embn7a.png')`,
+    backgroundSize: "c",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    animation: "moveBackground 25s linear infinite", 
+  };
+
+
+
+  const styles = `
+  @keyframes moveBackground {
+    0% {
+      background-position: 0% 0%;
+    }
+    25% {
+      background-position: 100% 0%; /* Move from left to right */
+    }
+    50% {
+      background-position: 100% 100%; /* Move from top to bottom */
+    }
+    75% {
+      background-position: 0% 100%; /* Move from right to left */
+    }
+    100% {
+      background-position: 0% 0%; /* Move from bottom to top */
+    }
+  }
+`;
+
+
+
+  // You can add the styles to the head of your HTML document
+  const styleSheet = document.styleSheets[0];
+  styleSheet.insertRule(styles, styleSheet.cssRules.length);
 
   return (
     <>
@@ -55,10 +87,44 @@ const AboutUsPage = () => {
             paddingTop: "200px",
           }}
         >
-          <a href="#" onClick={handleLocateUsClick}>
-            Locate Us
-          </a>
           <header className="about-us-header">
+            <div className="locate-us-container">
+              <h2 className="section-title">Locate Us</h2>
+              <div className="location-info">
+                <div className="location-details">
+                  <div className="address">
+                    <i className="fas fa-map-marker-alt"></i>
+                    <p>No 16 Randa Area,</p>
+                    <p>Ogbomoso, Oyo State, 210101</p>
+                  </div>
+                  <div className="phone">
+                    <i className="fas fa-phone"></i>
+                    <p>Phone: +2347033959586</p>
+                  </div>
+                  <div className="email">
+                    <i className="fas fa-envelope"></i>
+                    <p>Email: adeoluamole@gmail.com</p>
+                  </div>
+                </div>
+                <div className="map" style={mapStyle}>
+                  <a
+                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={handleLocateUsClick}
+                    style={{
+                      fontSize: "20px",
+                      fontWeight: "bold",
+                      textDecoration: "none",
+                      color: "#007BFF",
+                      display: "block",
+                    }}
+                  >
+                    View Our Location
+                  </a>
+                </div>
+              </div>
+            </div>
             <AboutUsCarousel />
             <div className="header-content">
               <h1>About Our School</h1>
@@ -89,7 +155,10 @@ const AboutUsPage = () => {
           </section>
 
           <section className="additional-image about-us-image1">
-            <img src="https://res.cloudinary.com/dn4gfzlhq/image/upload/v1694435026/images_12_uzsauy.jpg" alt="First Image" />
+            <img
+              src="https://res.cloudinary.com/dn4gfzlhq/image/upload/v1694435026/images_12_uzsauy.jpg"
+              alt="First Image"
+            />
             <div className="image-content">
               <h2>Our Campus</h2>
               <p>
@@ -99,7 +168,10 @@ const AboutUsPage = () => {
           </section>
 
           <section className="additional-image about-us-image2">
-            <img src="https://res.cloudinary.com/dn4gfzlhq/image/upload/v1694435027/images_17_tkrwco.jpg" alt="Second Image" />
+            <img
+              src="https://res.cloudinary.com/dn4gfzlhq/image/upload/v1694435027/images_17_tkrwco.jpg"
+              alt="Second Image"
+            />
             <div className="image-content">
               <h2>Our Facilities</h2>
               <p>
