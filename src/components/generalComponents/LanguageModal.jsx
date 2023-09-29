0d60526e6effb410848ba3f1d126c0e6069754fb
Adeolu Amole <adeoluamole@gmail.com>
2023-09-29 18:18:40 +0100

    Working on the project perfection

--- src/components/generalComponents/LanguageModal.jsx
+++ src/components/generalComponents/LanguageModal.jsx
@@ -14,60 +14,6 @@ const LanguageModal = ({ isOpen, onClose }) => {
   };
   return (
     <>
-      {/* <Modal show={isOpen} onHide={onClose} animation={true} centered>
-        <Modal.Header closeButton className="bg-danger text-white">
-          <Modal.Title className="text-uppercase">language</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="text-uppercase">
-          <p className="d-flex justify-content-between" onClick={() => changeLanguage("en")}>
-            <span className="my-auto">English</span>
-            <img
-              src="pic/america.png"
-              style={{ width: "30px", height: "30px", borderRadius: "10px" }}
-              alt=""
-            />
-          </p>
-          <hr />
-          <p className="d-flex justify-content-between" onClick={() => changeLanguage("es")}>
-            <span className="my-auto">Spanish</span>
-            <img
-              src="pic/spain.png"
-              style={{ width: "30px", height: "30px", borderRadius: "10px" }}
-              alt=""
-            />
-          </p>
-          <hr />
-          <p className="d-flex justify-content-between" onClick={() => changeLanguage("en")}>
-            <span className="my-auto">German</span>
-            <img
-              src="pic/germany.png"
-              style={{ width: "30px", height: "30px", borderRadius: "10px" }}
-              alt=""
-            />
-          </p>
-          <hr />
-          <p className="d-flex justify-content-between" onClick={() => changeLanguage("en")}>
-            <span className="my-auto">Chinese</span>
-            <img
-              src="pic/china.png"
-              style={{ width: "30px", height: "30px", borderRadius: "10px" }}
-              alt=""
-            />
-          </p>
-          <hr />
-          <p className="d-flex justify-content-between" onClick={() => changeLanguage("en")}>
-            <span className="my-auto">French</span>
-            <img
-              src="pic/france.png"
-              style={{ width: "30px", height: "30px", borderRadius: "10px" }}
-              alt=""
-            />
-          </p>
-          <hr />
-        </Modal.Body>
-      </Modal> */}
-
-
       <Modal show={isOpen} onHide={onClose} animation={true} centered>
         <Modal.Header closeButton className="bg-danger text-white">
           <Modal.Title className="text-uppercase">{t("language.title")}</Modal.Title>
