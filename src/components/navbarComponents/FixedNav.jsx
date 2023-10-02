cd9f0852e55bbd26f4fbb570a12a89a57d4dacfd
Adeolu Amole <adeoluamole@gmail.com>
2023-10-02 11:57:23 +0100

    Working on the project perfection

--- src/components/navbarComponents/FixedNav.jsx
+++ src/components/navbarComponents/FixedNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigate, useNavigate } from "react-router-dom";
+import { FiPlay } from "react-icons/fi";
 
 
 const FixedNav = () => {
@@ -28,7 +29,8 @@ const FixedNav = () => {
         }}
       >
       <div className="w-50 h-100 my-auto d-flex justify-content-start position-relative" >
-        <img src="/pic/adexlogo.png" alt="" className="my-auto" onClick={navigateToGame} style={{borderRadius: '3px'}} />
+      {/* <div className="my-auto text-uppercase px-2" onClick={navigateToGame} style={{borderRadius: '3px', cursor: 'pointer', backgroundColor: 'red', paddingTop: '3px', paddingBottom: '3px'}}><FiPlay className="my-auto" /> play game</div> */}
+        <img src="/pic/game_icon.png" alt="" className="my-auto" onClick={navigateToGame} style={{borderRadius: '3px', cursor: 'pointer', width: '80px', height: '50px'}} />
       </div>
         <div className="w-50 my-auto d-flex justify-content-end">
           <button className="btn btn-sm text-white px-3" style={{backgroundColor: '#7AA93C'}} onClick={navigateToShop}>Shop</button>
